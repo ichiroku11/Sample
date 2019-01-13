256f47052c4c655f7e76a7d28496641f90972a2f
ichiroku11 <toolbox0x10@gmail.com>
2019-01-13 09:30:26 +0900

    Update EnumerableTest

--- src/DotNetCore2x/SampleTest/EnumerableTest.cs
+++ src/DotNetCore2x/SampleTest/EnumerableTest.cs
@@ -27,5 +27,32 @@ namespace SampleTest {
 			// Assert
 			Assert.Equal(new[] { 2, 3, 4, 5 }, actual);
 		}
+
+		[Fact]
+		public void Min_非nullの空のシーケンスで呼び出すとInvalidOperationException() {
+			// 非nullの場合は例外
+			Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<int>().Min());
+
+			// null許容型の場合はnull
+			Assert.Null(Enumerable.Empty<int?>().Min());
+		}
+
+		[Fact]
+		public void Max_非nullの空のシーケンスで呼び出すとInvalidOperationException() {
+			// 非nullの場合は例外
+			Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<int>().Max());
+
+			// null許容型の場合はnull
+			Assert.Null(Enumerable.Empty<int?>().Max());
+		}
+
+		[Fact]
+		public void Average_非nullの空のシーケンスで呼び出すとInvalidOperationException() {
+			// 非nullの場合は例外
+			Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<int>().Average());
+
+			// null許容型の場合はnull
+			Assert.Null(Enumerable.Empty<int?>().Average());
+		}
 	}
 }
