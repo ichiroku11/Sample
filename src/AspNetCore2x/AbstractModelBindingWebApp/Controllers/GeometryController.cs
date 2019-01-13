d527c35125255f533df5891a956cd23badbfc0b0
ichiroku11 <toolbox0x10@gmail.com>
2019-01-13 22:41:23 +0900

    Add AbstractModelBindingWebApp 抽象クラスのモデルにバインドするサンプル

---
+++ src/AspNetCore2x/AbstractModelBindingWebApp/Controllers/GeometryController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbstractModelBindingWebApp.Controllers {
+	public class GeometryController : Controller {
+		public IActionResult Index() {
+			return View();
+		}
+	}
+}
---
+++ src/AspNetCore2x/AbstractModelBindingWebApp/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AbstractModelBindingWebApp {
+	public class Startup {
+		public void ConfigureServices(IServiceCollection services) {
+			// MVCで利用するサービスを登録する
+			services.AddMvc(options => {
+				// todo:
+				//options.ModelBinderProviders.Insert(0, new GeometryModelBinderProvider());
+			}).SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+			services.Configure<RouteOptions>(options => {
+				// URLは小文字に
+				options.LowercaseUrls = true;
+			});
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			}
+
+			// パイプラインにMVCのミドルウェアを追加する
+			app.UseMvc(routes => {
+				routes.MapRoute(
+					name: "geometry",
+					template: "{controller=Geometry}/{Action=index}/{id?}");
+			});
+		}
+	}
+}
