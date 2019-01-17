7582ed07660982115f2ea1cfc90add31934df45f
ichiroku11 <toolbox0x10@gmail.com>
2019-01-17 15:38:06 +0900

    Add Reactive Extensions Sample RangeとSubscribe

---
+++ src/DotNetCore2x/SampleTest/Reactive/ObservableTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SampleTest.Reactive {
+	public class ObservableTest {
+		private readonly ITestOutputHelper _output;
+
+		public ObservableTest(ITestOutputHelper output) {
+			_output = output;
+		}
+
+		[Fact]
+		public void Range_Subscribeを試す() {
+			// Arrange
+			var values = new List<int>();
+			var completed = false;
+
+			// Act
+			Observable.Range(0, 3).Subscribe(
+				value => {
+					_output.WriteLine($"onNext: {value}");
+					Assert.False(completed);
+
+					values.Add(value);
+				},
+				exception => {
+					// エラーが発生しないので呼ばれない
+					Assert.True(false);
+				},
+				() => {
+					_output.WriteLine($"onCompleted");
+					Assert.False(completed);
+
+					completed = true;
+				});
+
+			// Assert
+			Assert.Equal(new List<int>() { 0, 1, 2, }, values);
+			Assert.True(completed);
+		}
+	}
+}
