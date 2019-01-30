c5a3e74c5038c7c0438601e601fff9bb9c63b0c1
ichiroku11 <toolbox0x10@gmail.com>
2019-01-30 21:43:41 +0900

    Add Convert.ToInt32 test 2進数文字列 => 数字

---
+++ src/DotNetCore2x/SampleTest/ConvertTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SampleTest {
+	public class ConvertTest {
+		[Theory]
+		[InlineData("1", 1)]
+		[InlineData("0001", 1)]
+		[InlineData("10", 2)]
+		[InlineData("11111111", 255)]
+		public void Int32_2進数に変換できる(string text, int expected) {
+			// Arrange
+			// Act
+			var actual = Convert.ToInt32(text, 2);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData("2")]
+		[InlineData("a")]
+		public void Int32_2進数に変換できずFormatExceptionがスローされる(string text) {
+			// Arrange
+			// Act
+			// Assert
+			Assert.Throws<FormatException>(() => Convert.ToInt32(text, 2));
+		}
+	}
+}
