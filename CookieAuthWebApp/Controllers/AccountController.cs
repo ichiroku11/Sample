b8ce2b8dea8b34c961c8aa3e7854702a5ac6f4cd
ichiroku11 <toolbox0x10@gmail.com>
2019-02-14 13:37:17 +0900

    Update CookieAuthWebApp

---
+++ CookieAuthWebApp/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookieAuthWebApp.Controllers {
+	public class AccountController : Controller {
+		// ログインビュー
+		[AllowAnonymous]
+		public IActionResult Login() {
+			return View();
+		}
+
+		// ログインのポスト処理
+		[HttpPost]
+		[AllowAnonymous]
+		public async Task<IActionResult> Login(IFormCollection fromValues) {
+			// サインインに必要なプリンシパルを作る
+			// 本当ならユーザIDとパスワードからユーザを特定して・・・という処理が入るはず
+			var claims = new[] {
+				// 適当なユーザ名を登録しておく
+				new Claim(ClaimTypes.Name, "user01@example.jp"),
+			};
+			var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+			var principal = new ClaimsPrincipal(identity);
+
+			// サインイン
+			// 認証クッキーをレスポンスに追加
+			await HttpContext.SignInAsync(principal);
+
+			// ログインが必要なアクションにリダイレクト
+			return RedirectToAction("Index", "Home");
+		}
+
+		// ログアウト
+		[AllowAnonymous]
+		public async Task<IActionResult> Logout() {
+			// サインアウト
+			// 認証クッキーをレスポンスから削除
+			await HttpContext.SignOutAsync();
+
+			// ログインビューにリダイレクト
+			return RedirectToAction("Login");
+		}
+	}
+}
---
+++ CookieAuthWebApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookieAuthWebApp.Controllers {
+	public class HomeController : Controller {
+		public IActionResult Index() {
+			// ユーザ名を表示する
+			// User.Identity.Nameは"user01@example.jp"
+			return Content(User.Identity.Name);
+		}
+	}
+}
