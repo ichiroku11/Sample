8805e24791dbb12e6b3b49c7c136debb93ac14cb
ichiroku11 <toolbox0x10@gmail.com>
2019-02-21 23:06:35 +0900

    Add EnumTest Enum.IsDefined

---
+++ src/DotNetCore2x/SampleTest/EnumTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SampleTest {
+	public class EnumTest {
+		private enum Fruit {
+			Apple = 0,
+			Banana,
+		}
+
+		[Fact]
+		public void IsDefined_数値を識別できる() {
+			// Arrange
+			// Act
+			// Assert
+			Assert.True(Enum.IsDefined(typeof(Fruit), (int)Fruit.Apple));
+			Assert.False(Enum.IsDefined(typeof(Fruit), (int)Fruit.Banana + 1));
+		}
+
+		[Fact]
+		public void IsDefined_文字列を識別できる() {
+			// Arrange
+			// Act
+			// Assert
+			Assert.True(Enum.IsDefined(typeof(Fruit), "Apple"));
+		}
+	}
+}
