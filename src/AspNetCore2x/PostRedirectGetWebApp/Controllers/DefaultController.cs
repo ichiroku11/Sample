a47404c91f08d8ebfede7a4872731e0a9d9e9d5f
ichiroku11 <toolbox0x10@gmail.com>
2019-03-01 22:29:26 +0900

    Update PostRedirectGetWebApp

---
+++ src/AspNetCore2x/PostRedirectGetWebApp/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostRedirectGetWebApp.Controllers {
+	public class DefaultController : Controller {
+		public IActionResult Index() {
+			return new EmptyResult();
+		}
+	}
+}
