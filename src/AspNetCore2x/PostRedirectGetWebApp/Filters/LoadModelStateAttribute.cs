77e3f5f08525225e38dc92a83e98bd53d237ef63
ichiroku11 <toolbox0x10@gmail.com>
2019-03-02 09:25:38 +0900

    Update PostRedirectGetWebApp SaveModelStateAttribute/LoadModelStateAttribute

--- src/AspNetCore2x/PostRedirectGetWebApp/Filters/LoadModelStateAttribute.cs
+++ src/AspNetCore2x/PostRedirectGetWebApp/Filters/LoadModelStateAttribute.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace PostRedirectGetWebApp.Filters {
-	public class LoadModelStateAttribute {
+	public class LoadModelStateAttribute : ActionFilterAttribute {
+		public override void OnActionExecuting(ActionExecutingContext context) {
+			var controller = context.Controller as Controller;
+			if (controller == null) {
+				// ありえるの？
+				return;
+			}
+
+			// todo: TempDataから復元する
+		}
 	}
 }
--- src/AspNetCore2x/PostRedirectGetWebApp/Filters/SaveModelStateAttribute.cs
+++ src/AspNetCore2x/PostRedirectGetWebApp/Filters/SaveModelStateAttribute.cs
@@ -2,8 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace PostRedirectGetWebApp.Filters {
-	public class SaveModelStateAttribute {
+	public class SaveModelStateAttribute : ActionFilterAttribute {
+		private static bool IsRedirectResult(IActionResult result)
+			=> result is RedirectToActionResult || result is RedirectToRouteResult;
+
+		public override void OnActionExecuted(ActionExecutedContext context) {
+			// リダイレクトのみが対象
+			if (!IsRedirectResult(context.Result)) {
+				return;
+			}
+
+			var controller = context.Controller as Controller;
+			if (controller == null) {
+				// ありえるの？
+				return;
+			}
+
+			// バリデーションに失敗した場合が対象
+			if (controller.ModelState.IsValid) {
+				return;
+			}
+
+			// todo: TempDataに保存する
+		}
 	}
 }
