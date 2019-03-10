625eec5e1cb680042a93cd4dda5f17ee03766a90
ichiroku11 <toolbox0x10@gmail.com>
2019-03-10 11:00:19 +0900

    Add CancelConsoleApp EfCore2xといいつつ、SqlCommandのキャンセルを試す

---
+++ src/EfCore2x/CancelConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace CancelConsoleApp {
+	class Program {
+		private static readonly string _connectionString
+			= new SqlConnectionStringBuilder {
+				DataSource = ".",
+				InitialCatalog = "Test",
+				IntegratedSecurity = true,
+			}.ToString();
+
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+
+			using(var connection = new SqlConnection(_connectionString)) {
+				var sql = "select 100;";
+				var result = connection.Query<int>(sql).First();
+				Console.WriteLine(result);
+			}
+		}
+	}
+}
