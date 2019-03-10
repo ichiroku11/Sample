4b7809f2d2799f1faa9d5938e3d3cb8dd929c8ed
ichiroku11 <toolbox0x10@gmail.com>
2019-03-10 15:49:26 +0900

    Update CancelConsoleApp

--- src/EfCore2x/CancelConsoleApp/Program.cs
+++ src/EfCore2x/CancelConsoleApp/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Linq;
-using Dapper;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CancelConsoleApp {
 	class Program {
@@ -12,14 +13,41 @@ namespace CancelConsoleApp {
 				IntegratedSecurity = true,
 			}.ToString();
 
-		static void Main(string[] args) {
+		private static async Task<int> Run(CancellationToken token) {
+			using (var connection = new SqlConnection(_connectionString)) {
+				await connection.OpenAsync(token);
+
+				var sql = @"
+waitfor delay '00:00:05';
+select 100;";
+				using (var command = new SqlCommand(sql, connection))
+				using (var reader = await command.ExecuteReaderAsync(token)) {
+					while (await reader.ReadAsync(token)) {
+						var value = await reader.GetFieldValueAsync<int>(0, token);
+
+						return value;
+					}
+				}
+			}
+
+			return -1;
+		}
+
+
+		static async Task Main(string[] args) {
 			Console.WriteLine("Hello World!");
 
-			using(var connection = new SqlConnection(_connectionString)) {
-				var sql = "select 100;";
-				var result = connection.Query<int>(sql).First();
-				Console.WriteLine(result);
+			var result = await Run(CancellationToken.None);
+			Console.WriteLine(result);
+
+			/*
+			var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+			try {
+				await Run(tokenSource.Token);
+			} catch (Exception) {
+				throw;
 			}
+			*/
 		}
 	}
 }
