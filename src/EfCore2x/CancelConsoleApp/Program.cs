a23c1e8cf169ded846773e92f43ef2a509a9bd02
ichiroku11 <toolbox0x10@gmail.com>
2019-03-11 22:36:00 +0900

    Update CancelConsoleApp

--- src/EfCore2x/CancelConsoleApp/Program.cs
+++ src/EfCore2x/CancelConsoleApp/Program.cs
@@ -13,19 +13,30 @@ namespace CancelConsoleApp {
 				IntegratedSecurity = true,
 			}.ToString();
 
-		private static async Task<int> Run(CancellationToken token) {
+		private static async Task<int> QueryAsync(CancellationToken token) {
 			using (var connection = new SqlConnection(_connectionString)) {
 				await connection.OpenAsync(token);
 
 				var sql = @"
+-- CancelTest
 waitfor delay '00:00:05';
 select 100;";
-				using (var command = new SqlCommand(sql, connection))
-				using (var reader = await command.ExecuteReaderAsync(token)) {
-					while (await reader.ReadAsync(token)) {
-						var value = await reader.GetFieldValueAsync<int>(0, token);
 
-						return value;
+				using (var command = new SqlCommand(sql, connection)) {
+					try {
+						Console.WriteLine("ExecuteReaderAsync");
+						using (var reader = await command.ExecuteReaderAsync(token)) {
+
+							Console.WriteLine("ReadAsync");
+							while (await reader.ReadAsync(token)) {
+								var value = await reader.GetFieldValueAsync<int>(0, token);
+
+								return value;
+							}
+						}
+					} catch (Exception exception) {
+						Console.WriteLine("catch:");
+						Console.WriteLine(exception);
 					}
 				}
 			}
@@ -33,21 +44,19 @@ select 100;";
 			return -1;
 		}
 
-
 		static async Task Main(string[] args) {
-			Console.WriteLine("Hello World!");
-
-			var result = await Run(CancellationToken.None);
-			Console.WriteLine(result);
-
-			/*
-			var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(3));
-			try {
-				await Run(tokenSource.Token);
-			} catch (Exception) {
-				throw;
+			// キャンセルしない
+			{
+				await QueryAsync(CancellationToken.None);
 			}
-			*/
+
+			// キャンセルする
+			{
+				var timeout = TimeSpan.FromSeconds(2);
+				var tokenSource = new CancellationTokenSource(timeout);
+
+				await QueryAsync(tokenSource.Token);
+			}
 		}
 	}
 }
