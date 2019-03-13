5b8a8f5d1bd60a5a8c5b7c5adaf2353af6b94bc1
ichiroku11 <toolbox0x10@gmail.com>
2019-03-13 22:57:04 +0900

    Update SampleTest Stack.Pop

---
+++ src/DotNetCore2x/SampleTest/StackTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SampleTest {
+	public class StackTest {
+		[Fact]
+		public void Pop_コンストラクタで指定したコレクションの逆順で取得できる() {
+			// Arrange
+			// Act
+			var stack = new Stack<int>(new[] { 1, 2, 3 });
+
+			// Assert
+			Assert.Equal(3, stack.Pop());
+			Assert.Equal(2, stack.Pop());
+			Assert.Equal(1, stack.Pop());
+		}
+	}
+}
