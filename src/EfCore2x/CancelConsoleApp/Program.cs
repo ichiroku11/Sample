06d4a4c3e4ab4b5d96201cfe2080598948fc668c
ichiroku11 <toolbox0x10@gmail.com>
2019-03-16 08:56:34 +0900

    Refactor

--- src/EfCore2x/CancelConsoleApp/Program.cs
+++ src/EfCore2x/CancelConsoleApp/Program.cs
@@ -13,53 +13,22 @@ namespace CancelConsoleApp {
 				IntegratedSecurity = true,
 			}.ToString();
 
-
-		private static async Task<int> QueryAsync(CancellationToken token) {
-			using (var connection = new SqlConnection(_connectionString)) {
-				Console.WriteLine(nameof(SqlConnection.OpenAsync));
-				await connection.OpenAsync(token);
-
-				// 5秒待って値を取得する
-				var sql = @"
--- CancelTest
-waitfor delay '00:00:05';
-select 100;";
-
-				using (var command = new SqlCommand(sql, connection)) {
-					try {
-						Console.WriteLine(nameof(SqlCommand.ExecuteReaderAsync));
-						using (var reader = await command.ExecuteReaderAsync(token)) {
-
-							Console.WriteLine(nameof(SqlDataReader.ReadAsync));
-							await reader.ReadAsync(token);
-
-							Console.WriteLine(nameof(SqlDataReader.GetFieldValueAsync));
-							return await reader.GetFieldValueAsync<int>(0, token);
-						}
-					} catch (Exception exception) {
-						// todo:
-						Console.WriteLine("catch:");
-						Console.WriteLine(exception);
-					}
-				}
-			}
-
-			return -1;
-		}
-
 		static async Task Main(string[] args) {
 			{
 				// キャンセルしない
-				await QueryAsync(CancellationToken.None);
+				var result = await new SampleQuery(_connectionString).RunAsync(CancellationToken.None);
+				Console.WriteLine(result);
 			}
 
 			{
 				// キャンセルする
+
 				// 2秒後
 				var timeout = TimeSpan.FromSeconds(2);
 				var tokenSource = new CancellationTokenSource(timeout);
 
-				await QueryAsync(tokenSource.Token);
+				var result = await new SampleQuery(_connectionString).RunAsync(tokenSource.Token);
+				Console.WriteLine(result);
 			}
 		}
 	}
---
+++ src/EfCore2x/CancelConsoleApp/SampleQuery.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancelConsoleApp {
+	// サンプルクエリ
+	public class SampleQuery {
+		// パラメータを使わないSqlCommandを生成
+		private static SqlCommand CreateCommandNoParameter(SqlConnection connection) {
+			// 5s待機して1を取得
+			const string sql = @"
+-- CancelTest
+waitfor delay '00:00:05';
+select 1;";
+
+			return new SqlCommand(sql, connection);
+		}
+
+		// パラメータを使うSqlCommandを生成
+		private static SqlCommand CreateCommandWithParameter(SqlConnection connection) {
+			// 5s待機して2を取得
+			const string sql = @"
+-- CancelTest
+waitfor delay '00:00:05';
+select @p;";
+
+			var command = new SqlCommand(sql, connection);
+			command.Parameters.AddWithValue("@p", 2);
+			return command;
+		}
+
+
+		private readonly string _connectionString;
+		private readonly bool _useParameter;
+
+
+		public SampleQuery(string connectionString, bool useParameter = false) {
+			_connectionString = connectionString;
+			_useParameter = useParameter;
+		}
+
+
+		private SqlCommand CreateCommand(SqlConnection connection) {
+			return _useParameter
+				? CreateCommandWithParameter(connection)
+				: CreateCommandNoParameter(connection);
+		}
+
+		// クエリを実行する
+		public async Task<int> RunAsync(CancellationToken token) {
+			using (var connection = new SqlConnection(_connectionString)) {
+				// コネクションを開く
+				await connection.OpenAsync(token);
+
+				using (var command = CreateCommand(connection)) {
+					try {
+						// SQLを実行
+						Console.WriteLine(nameof(SqlCommand.ExecuteReaderAsync));
+						using (var reader = await command.ExecuteReaderAsync(token)) {
+							// 結果を読み込む
+							Console.WriteLine(nameof(SqlDataReader.ReadAsync));
+							await reader.ReadAsync(token);
+
+							// とりあえず1行目だけを読み込む
+							Console.WriteLine(nameof(SqlDataReader.GetFieldValueAsync));
+							return await reader.GetFieldValueAsync<int>(0, token);
+						}
+					} catch (Exception exception) {
+						Console.WriteLine(exception);
+					}
+				}
+			}
+
+			return -1;
+		}
+
+		// クエリを実行
+		public Task<int> RunAsync() => RunAsync(CancellationToken.None);
+	}
+}
