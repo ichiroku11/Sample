bed7fd79a79ee572ffd90118aaaa634a8a6ea5ca
ichiroku11 <toolbox0x10@gmail.com>
2019-03-23 08:50:37 +0900

    Update BasicAuthWebApp たぶんこのあたりを実装する

---
+++ src/AspNetCore2x/BasicAuthWebApp/BasicAuthenticationDefaults.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuthWebApp {
+	public static class BasicAuthenticationDefaults {
+		public const string AuthenticationScheme = "Basic";
+	}
+}
---
+++ src/AspNetCore2x/BasicAuthWebApp/BasicAuthenticationHandler.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace BasicAuthWebApp {
+	public class BasicAuthenticationHandler : AuthenticationHandler<BasicAuthenticationOptions> {
+		public BasicAuthenticationHandler(
+			IOptionsMonitor<BasicAuthenticationOptions> options,
+			ILoggerFactory logger,
+			UrlEncoder encoder,
+			ISystemClock clock)
+			: base(options, logger, encoder, clock) {
+		}
+
+		protected override Task<AuthenticateResult> HandleAuthenticateAsync() {
+			throw new NotImplementedException();
+		}
+	}
+}
