f0d71128a8468873eeac13d84365fd7b40ed6ffd
ichiroku11 <toolbox0x10@gmail.com>
2019-03-30 22:22:10 +0900

    Update CookieAuthWebApp.Test HomeIndex_Get_* - 認証されていないときのリダイレクト AccountLogin_Post_* - 認証してリダイレクト

--- src/AspNetCore2x/CookieAuthWebApp.Test/CookieAuthWebAppTest.cs
+++ src/AspNetCore2x/CookieAuthWebApp.Test/CookieAuthWebAppTest.cs
@@ -7,24 +7,15 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace CookieAuthWebApp.Test {
-	public class CookieAuthWebAppTest : IClassFixture<WebApplicationFactory<Startup>>, IDisposable {
-		private static HttpContent GetEmptyContent()
-			=> new FormUrlEncodedContent(new Dictionary<string, string>());
-
+	public class CookieAuthWebAppTest : IClassFixture<WebApplicationFactory<Startup>> {
 		private readonly WebApplicationFactory<Startup> _factory;
 
 		public CookieAuthWebAppTest(WebApplicationFactory<Startup> factory) {
 			_factory = factory;
 		}
 
-		private Uri BaseAddress => _factory.Server.BaseAddress;
-
-		public void Dispose() {
-			_factory.Dispose();
-		}
-
 		[Fact]
-		public async Task HomeIndex_GETリクエストを送るとログインページへリダイレクト() {
+		public async Task HomeIndex_Get_AccountLoginへリダイレクト() {
 			var options = new WebApplicationFactoryClientOptions {
 				AllowAutoRedirect = false,
 			};
@@ -33,8 +24,36 @@ namespace CookieAuthWebApp.Test {
 
 				Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
 				Assert.Equal(
-					new Uri(BaseAddress, "/account/login").ToString(),
+					new Uri(_factory.Server.BaseAddress, "/account/login").ToString(),
 					response.Headers.Location.GetLeftPart(UriPartial.Path));
+			}
+		}
+
+		[Fact]
+		public async Task AccountLogin_Post_HomeIndexへリダイレクト() {
+			var options = new WebApplicationFactoryClientOptions {
+				AllowAutoRedirect = false,
+			};
+			using (var clinet = _factory.CreateClient(options)) {
+				var response = await clinet.PostAsync(
+					"/account/login",
+					new FormUrlEncodedContent(new Dictionary<string, string>()));
+
+				Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+				Assert.Equal("/", response.Headers.Location.ToString());
+			}
+		}
+
+		[Fact]
+		public async Task AccountLogin_Post_HomeIndexへリダイレクトして取得できる() {
+			using (var clinet = _factory.CreateClient()) {
+				var response = await clinet.PostAsync(
+					"/account/login",
+					new FormUrlEncodedContent(new Dictionary<string, string>()));
+				Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+				var content = await response.Content.ReadAsStringAsync();
+				Assert.Equal("user01@example.jp", content);
 			}
 		}
 	}
