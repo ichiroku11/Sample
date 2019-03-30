bf7742eb07159f481aa56fbdaad2777e690a7d03
ichiroku11 <toolbox0x10@gmail.com>
2019-03-30 09:44:36 +0900

    Update BasicAuthWebApp

--- src/AspNetCore2x/BasicAuthWebApp/BasicAuthenticationHandler.cs
+++ src/AspNetCore2x/BasicAuthWebApp/BasicAuthenticationHandler.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
 
 namespace BasicAuthWebApp {
 	public class BasicAuthenticationHandler : AuthenticationHandler<BasicAuthenticationOptions> {
+		private class HeaderNames {
+			public const string WwwAuthenticate = "WWW-Authenticate";
+			public const string Authorization = "Authorization";
+		}
+
 		public BasicAuthenticationHandler(
 			IOptionsMonitor<BasicAuthenticationOptions> options,
 			ILoggerFactory logger,
@@ -18,8 +25,27 @@ namespace BasicAuthWebApp {
 		}
 
 		protected override Task<AuthenticateResult> HandleAuthenticateAsync() {
-			// todo:
 			return Task.FromResult(AuthenticateResult.NoResult());
+			// todo:
+			/*
+			var header = Request.Headers[HeaderNames.Authorization];
+			if (StringValues.IsNullOrEmpty(header)) {
+				// todo:
+				return Task.FromResult(AuthenticateResult.NoResult());
+			}
+			*/
+		}
+
+
+		protected override Task HandleChallengeAsync(AuthenticationProperties properties) {
+			return base.HandleChallengeAsync(properties);
+			// todo:
+			/*
+			Response.StatusCode = 401;
+			Response.Headers.Append(HeaderNames.WwwAuthenticate, "Basic");
+
+			return Task.CompletedTask;
+			*/
 		}
 	}
 }
