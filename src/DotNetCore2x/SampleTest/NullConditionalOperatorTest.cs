159764a7ec3d9c1bb2cfca6ac87c58a09fafbb33
ichiroku11 <toolbox0x10@gmail.com>
2019-04-16 22:35:34 +0900

    Update SampleTest

---
+++ src/DotNetCore2x/SampleTest/NullConditionalOperatorTest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SampleTest {
+	// null条件演算子のテスト
+	public class NullConditionalOperatorTest {
+		[Fact(Skip = "何をテストしようか")]
+		public void Null条件演算子_何を試そう() {
+			/*
+			var action = default(Action);
+
+			action?.Invoke();
+			*/
+		}
+	}
+}
