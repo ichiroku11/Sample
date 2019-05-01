a91322734dc289a1f732b3b8890a2818d86516dc
ichiroku11 <toolbox0x10@gmail.com>
2019-05-01 18:22:54 +0900

    Update GenericHostConsoleApp 手探りで試してる どうもIHostedServiceを使うっぽい Nugetパッケージは個別に入れるのかな？

--- src/DotNetCore2x/GenericHostConsoleApp/Program.cs
+++ src/DotNetCore2x/GenericHostConsoleApp/Program.cs
@@ -1,8 +1,31 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GenericHostConsoleApp {
+	class SampleService : IHostedService {
+		private readonly ILogger _logger;
+
+		public SampleService(ILogger<SampleService> logger) {
+			_logger = logger;
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken) {
+			_logger.LogInformation(nameof(StartAsync));
+
+			return Task.CompletedTask;
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken) {
+			_logger.LogInformation(nameof(StopAsync));
+
+			return Task.CompletedTask;
+		}
+	}
+
 	class Program {
 		static async Task Main(string[] args) {
 			Console.WriteLine("Hello World!");
@@ -11,12 +34,24 @@ namespace GenericHostConsoleApp {
 			await new HostBuilder()
 				.ConfigureAppConfiguration((context, config) => {
 					// todo: json
+					// todo: WebHost.cs
+					/*
+					var env = hostingContext.HostingEnvironment;
+
+					config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+						  .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+					*/
 				})
 				.ConfigureServices(services => {
 					// todo:
+					services.AddHostedService<SampleService>();
 				})
 				.ConfigureLogging((context, logging) => {
-					// todo:
+					// todo: WebHost.cs
+					logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+					logging.AddConsole();
+					logging.AddDebug();
+					//logging.AddEventSourceLogger();
 				})
 				.RunConsoleAsync();
 		}
