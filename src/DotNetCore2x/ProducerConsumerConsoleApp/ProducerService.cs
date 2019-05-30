2c345d36116bd5aff94ea5722f4a9b6127638333
ichiroku11 <toolbox0x10@gmail.com>
2019-05-30 23:21:36 +0900

    Update ProducerConsumerConsoleApp

---
+++ src/DotNetCore2x/ProducerConsumerConsoleApp/ProducerService.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumerConsoleApp {
+	public class ProducerService : IHostedService {
+		public Task StartAsync(CancellationToken cancellationToken) {
+			return Task.CompletedTask;
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken) {
+			return Task.CompletedTask;
+		}
+	}
+}
--- src/DotNetCore2x/ProducerConsumerConsoleApp/Program.cs
+++ src/DotNetCore2x/ProducerConsumerConsoleApp/Program.cs
@@ -1,12 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
+using System.Threading.Tasks;
 
 namespace ProducerConsumerConsoleApp {
 	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+		static async Task Main(string[] args) {
+			// 参考
+			// https://docs.microsoft.com/ja-jp/aspnet/core/fundamentals/host/hosted-services?view=aspnetcore-2.2
+			// https://docs.microsoft.com/ja-jp/dotnet/standard/microservices-architecture/multi-container-microservice-net-applications/background-tasks-with-ihostedservice
 
-
-
+			await new HostBuilder()
+				.ConfigureAppConfiguration((context, config) => {
+				})
+				.ConfigureServices(services => {
+					services
+						.AddHostedService<ProducerService>()
+						.AddHostedService<ConsumerService>();
+				})
+				.RunConsoleAsync();
 		}
 	}
 }
