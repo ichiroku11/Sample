889e66ed01c97b80e1de273e7f564be5dddb5289
ichiroku11 <toolbox0x10@gmail.com>
2019-06-01 09:11:53 +0900

    Update ProducerConsumerConsoleApp Update BlockingQueue

--- src/DotNetCore2x/ProducerConsumerConsoleApp/BlockingQueue.cs
+++ src/DotNetCore2x/ProducerConsumerConsoleApp/BlockingQueue.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
@@ -6,14 +7,25 @@ using System.Threading.Tasks;
 
 namespace ProducerConsumerConsoleApp {
 	public class BlockingQueue<TItem> : IBlockingQueue<TItem> {
+		private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(0);
+		private readonly ConcurrentQueue<TItem> _queue = new ConcurrentQueue<TItem>();
+
 		public void Enqueue(TItem item) {
-			// todo:
-			throw new NotImplementedException();
-		}
-		public Task<TItem> DequeueAsync(CancellationToken token = default) {
-			// todo:
-			throw new NotImplementedException();
+			// キューに追加する
+			_queue.Enqueue(item);
+
+			// セマフォを解放して通知する
+			_semaphore.Release();
 		}
 
+		public async Task<TItem> DequeueAsync(CancellationToken token = default) {
+			// セマフォが解放されるまで待機する
+			await _semaphore.WaitAsync(token);
+
+			// キューから取り出す
+			_queue.TryDequeue(out var item);
+
+			return item;
+		}
 	}
 }
