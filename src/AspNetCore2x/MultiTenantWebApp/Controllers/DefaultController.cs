f153bef18b125f8393289f7fc54e21890a926338
ichiroku11 <toolbox0x10@gmail.com>
2019-09-15 21:56:28 +0900

    Update MultiTenantWebApp

--- src/AspNetCore2x/MultiTenantWebApp/Controllers/DefaultController.cs
+++ src/AspNetCore2x/MultiTenantWebApp/Controllers/DefaultController.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MultiTenantWebApp.Models;
 
 namespace MultiTenantWebApp.Controllers {
 	public class DefaultController : Controller {
-		public IActionResult Index() {
-			// ログイン済み
-			return Content(User.Identity.Name);
+		// 本当ならコントローラは直接DBコンテキストを持たないけど
+		private readonly AppDbContext _dbContext;
+
+		public DefaultController(AppDbContext dbContext) {
+			_dbContext = dbContext;
+		}
+
+		public IActionResult Index(bool all = false) {
+			var blogs = all
+				// グローバルフィルタを無視する
+				? _dbContext.Blogs.IgnoreQueryFilters()
+				// グローバルフィルタ
+				: _dbContext.Blogs;
+
+			return View(blogs.ToList());
 		}
 	}
 }
