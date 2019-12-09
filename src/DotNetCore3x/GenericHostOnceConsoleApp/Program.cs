acedc080ba129fbbded45f52202b7f9a0881fd64
ichiroku11 <toolbox0x10@gmail.com>
2019-12-09 13:13:12 +0900

    Update GenericHostOnceConsoleApp

--- src/DotNetCore3x/GenericHostOnceConsoleApp/Program.cs
+++ src/DotNetCore3x/GenericHostOnceConsoleApp/Program.cs
@@ -1,9 +1,35 @@
-﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace GenericHostOnceConsoleApp {
 	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+		static async Task Main(string[] args) {
+			var builder = new HostBuilder()
+				.ConfigureServices(services => {
+					services.Configure<ConsoleLifetimeOptions>(options => {
+						// メッセージを抑制する
+						options.SuppressStatusMessages = true;
+					});
+
+					// 実行するサービスを登録
+					services.AddHostedService<SampleService>();
+				})
+				.ConfigureLogging(logging => {
+					// コンソールにログを残す
+					logging.AddConsole();
+				});
+
+			// ホストを開始して、サービスの実行が終わったら、ホストも終了する
+			using var host = builder.Build();
+
+			// 開始する
+			await host.StartAsync();
+
+			// 終了する
+			await host.StopAsync();
 		}
 	}
 }
