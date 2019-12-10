5626cb1a126079f1520a9f2edaa090ecff15e4c1
ichiroku11 <toolbox0x10@gmail.com>
2019-12-10 23:15:56 +0900

    Update GenericHostConsoleApp

--- src/DotNetCore3x/GenericHostConsoleApp/Program.cs
+++ src/DotNetCore3x/GenericHostConsoleApp/Program.cs
@@ -1,9 +1,24 @@
-﻿using System;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace GenericHostConsoleApp {
 	class Program {
-		static void Main(string[] args) {
+		static async Task Main(string[] args) {
 			Console.WriteLine("Hello World!");
+
+			var builder = new HostBuilder();
+
+
+
+			// 以下は
+			// await builder.RunConsoleAsync();
+			// と同じっぽい
+			builder.UseConsoleLifetime();
+
+			using var host = builder.Build();
+
+			await host.RunAsync();
 		}
 	}
 }
