a25f4b056f624f120819bd91f940b982cd61b14d
ichiroku11 <toolbox0x10@gmail.com>
2019-12-26 09:35:24 +0900

    Update TagHelperWebApp Update PrePostTagHelper

--- src/AspNetCore3x/TagHelperWebApp/TagHelpers/PrePostTagHelper.cs
+++ src/AspNetCore3x/TagHelperWebApp/TagHelpers/PrePostTagHelper.cs
@@ -7,12 +7,19 @@ using System.Threading.Tasks;
 namespace TagHelperWebApp.TagHelpers {
 	public class PrePostTagHelper : TagHelper {
 		public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output) {
+			// div要素に変換
 			output.TagName = "div";
 
-			output.PreElement.AppendHtml($"<span>{nameof(output.PreElement)}</span>");
-			output.PreContent.AppendHtml($"<span>{nameof(output.PreContent)}</span>");
-			output.PostContent.AppendHtml($"<span>{nameof(output.PostContent)}</span>");
-			output.PostElement.AppendHtml($"<span>{nameof(output.PostElement)}</span>");
+			// div要素にclass属性を設定
+			output.Attributes.Add("class", "content");
+
+			// コンテンツ"内"の前後にhtmlを挿入
+			output.PreContent.SetHtmlContent($"<span class=\"pre-content\">{nameof(output.PreContent)}</span>");
+			output.PostContent.SetHtmlContent($"<span class=\"post-content\">{nameof(output.PostContent)}</span>");
+
+			// コンテンツ"外"の前後にhtmlを挿入
+			output.PreElement.SetHtmlContent($"<div class=\"pre-element\">{nameof(output.PreElement)}</div>");
+			output.PostElement.SetHtmlContent($"<div class=\"post-element\">{nameof(output.PostElement)}</div>");
 
 			return Task.CompletedTask;
 		}
