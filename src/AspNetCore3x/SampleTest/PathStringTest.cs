79468497e4e63884de766ed29d031b32063fc79b
ichiroku11 <toolbox0x10@gmail.com>
2019-12-29 16:10:13 +0900

    Migrate SampleTest 2.x => 3.x PathStringTest/UriHelperTest

---
+++ src/AspNetCore3x/SampleTest/PathStringTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Xunit;
+
+namespace SampleTest {
+	public class PathStringTest {
+		[Fact]
+		public void PathString_とりあえず使ってみる() {
+			// Arrange
+			// Act
+			var path = new PathString("/app");
+
+			// Assert
+			Assert.Equal("/app", path.Value);
+		}
+
+		[Fact(DisplayName = "PathString_コンストラクタ引数は'/'で始まらないとArgumentExceptionがスローされる")]
+		public void PathString_ConstructorArgumentStartWithSlash() {
+			// Arrange
+			// Act
+			Assert.Throws<ArgumentException>(() => {
+				new PathString("app");
+			});
+
+			// Assert
+			//Assert.Equal("/app", path.Value);
+		}
+	}
+}
