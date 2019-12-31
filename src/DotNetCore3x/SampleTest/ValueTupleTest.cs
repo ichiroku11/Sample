3e02a8e56da5b0a801cebb1072702e48c3040018
ichiroku11 <toolbox0x10@gmail.com>
2019-12-31 22:50:28 +0900

    Add ValueTupleTest

---
+++ src/DotNetCore3x/SampleTest/ValueTupleTest.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SampleTest {
+	public class ValueTupleTest {
+		[Fact(DisplayName = "名前がないタプルはItem1、Item2といったフィルード名でアクセスできる")]
+		public void ValueTuple_Unnamed() {
+			var unnamed = ("a", 1);
+
+			Assert.Equal("a", unnamed.Item1);
+			Assert.Equal(1, unnamed.Item2);
+		}
+
+		[Fact(DisplayName = "名前付きタプルは指定したフィールド名でアクセスできる")]
+		public void ValueTuple_Named() {
+			var named = (first: "a", second: 1);
+
+			Assert.Equal("a", named.first);
+			Assert.Equal(1, named.second);
+		}
+	}
+}
