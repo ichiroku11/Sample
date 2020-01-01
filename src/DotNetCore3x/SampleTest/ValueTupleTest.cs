9185cebc09c661fe89af4971c09220524564e684
ichiroku11 <toolbox0x10@gmail.com>
2020-01-01 10:46:13 +0900

    Update ValueTupleTest

--- src/DotNetCore3x/SampleTest/ValueTupleTest.cs
+++ src/DotNetCore3x/SampleTest/ValueTupleTest.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace SampleTest {
 	public class ValueTupleTest {
-		[Fact(DisplayName = "名前がないタプルはItem1、Item2といったフィルード名でアクセスできる")]
+		[Fact(DisplayName = "ValueTuple_名前がないタプルはItem1、Item2といったフィルード名でアクセスできる")]
 		public void ValueTuple_Unnamed() {
 			var unnamed = ("a", 1);
 
@@ -13,7 +13,7 @@ namespace SampleTest {
 			Assert.Equal(1, unnamed.Item2);
 		}
 
-		[Fact(DisplayName = "名前付きタプルは指定したフィールド名でアクセスできる")]
+		[Fact(DisplayName = "ValueTuple_名前付きタプルは指定したフィールド名でアクセスできる")]
 		public void ValueTuple_Named() {
 			var named = (first: "a", second: 1);
 
@@ -23,6 +23,19 @@ namespace SampleTest {
 			// Item1、Item2といったフィールド名でもアクセスできる
 			Assert.Equal("a", named.Item1);
 			Assert.Equal(1, named.Item2);
+		}
+
+		[Fact]
+		public void ValueTuple_プロジェクション初期化子() {
+			var x = 1;
+			var y = 2;
+
+			// プロジェクション初期化子
+			// 暗黙的な名前が射影されるというやつ
+			var tuple = (x, y);
+
+			Assert.Equal(x, tuple.x);
+			Assert.Equal(y, tuple.y);
 		}
 	}
 }
