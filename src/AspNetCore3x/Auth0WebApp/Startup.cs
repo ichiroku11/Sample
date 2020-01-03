82f6723d8a96623be9d46855f342a33d2a71baba
ichiroku11 <toolbox0x10@gmail.com>
2020-01-03 10:48:13 +0900

    Update Auth0WebApp

--- src/AspNetCore3x/Auth0WebApp/Startup.cs
+++ src/AspNetCore3x/Auth0WebApp/Startup.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Auth0WebApp {
 	// 参考
@@ -30,13 +32,31 @@ namespace Auth0WebApp {
 				.AddCookie()
 				// OpenID Connectによる認証
 				.AddOpenIdConnect(authenticationScheme: "Auth0", options => {
+					// appsettings.jsonのAuth0オプション
 					var auth0Options = _config.GetSection("Auth0").Get<Auth0Options>();
 
+					// Authority
 					options.Authority = auth0Options.Authority;
+
+					// Client ID & Client Secret
 					options.ClientId = auth0Options.ClientId;
 					options.ClientSecret = auth0Options.ClientSecret;
-					// todo:
 
+					options.CallbackPath = new PathString("/callback");
+					options.ClaimsIssuer = "Auth0";
+					options.ResponseType = OpenIdConnectResponseType.Code;
+					options.Scope.Add("openid");
+
+					options.Events = new OpenIdConnectEvents {
+						OnRedirectToIdentityProviderForSignOut = (context) => {
+
+							// todo:
+
+							return Task.CompletedTask;
+						},
+					};
+
+					// todo:
 
 				});
 		}
