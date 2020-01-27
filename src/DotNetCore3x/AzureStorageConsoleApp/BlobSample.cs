544a1ead079d9b053839645c627bac93a7d77c5c
ichiroku11 <toolbox0x10@gmail.com>
2020-01-27 20:25:44 +0900

    AzureStorageConsoleApp: Update BlobSample

--- src/DotNetCore3x/AzureStorageConsoleApp/BlobSample.cs
+++ src/DotNetCore3x/AzureStorageConsoleApp/BlobSample.cs
@@ -7,16 +7,19 @@ using System.Threading.Tasks;
 
 namespace AzureStorageConsoleApp {
 	public class BlobSample {
-		//private readonly string _connectionString;
+		private readonly string _connectionString;
 		private readonly ILogger _logger;
 
 		public BlobSample(IConfiguration config, ILogger<BlobSample> logger) {
-			//_connectionString = config.GetConnectionString("Storage");
+			_connectionString = config.GetConnectionString("Storage");
 			_logger = logger;
 		}
 
 		public Task RunAsync() {
 			_logger.LogInformation(nameof(RunAsync));
+
+			// https://docs.microsoft.com/ja-jp/azure/storage/blobs/storage-quickstart-blobs-dotnet
+
 
 			return Task.CompletedTask;
 		}
