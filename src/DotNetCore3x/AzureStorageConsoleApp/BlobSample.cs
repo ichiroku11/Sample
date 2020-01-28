7947689becc9e7ea4b5bd9fcecf151c67e97d96e
ichiroku11 <toolbox0x10@gmail.com>
2020-01-28 23:05:46 +0900

    AzureStorageConsoleApp: Update BlobSample

--- src/DotNetCore3x/AzureStorageConsoleApp/BlobSample.cs
+++ src/DotNetCore3x/AzureStorageConsoleApp/BlobSample.cs
@@ -1,8 +1,10 @@
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,22 +18,54 @@ namespace AzureStorageConsoleApp {
 			_logger = logger;
 		}
 
+		private async Task UpoladBlobAsync(
+			BlobContainerClient containerClient, string blobName, string blobContent) {
+			using var stream = new MemoryStream(Encoding.UTF8.GetBytes(blobContent));
+
+			var blobClient = containerClient.GetBlobClient(blobName);
+
+			var exists = await blobClient.ExistsAsync();
+			if (exists) {
+				return;
+			}
+
+			var response = await blobClient.UploadAsync(stream);
+		}
+
+		private async Task<string> DownloadBlobAsync(
+			BlobContainerClient containerClient, string blobName) {
+
+			var blobClient = containerClient.GetBlobClient(blobName);
+			var response = await blobClient.DownloadAsync();
+
+			using var reader = new StreamReader(response.Value.Content);
+			return await reader.ReadToEndAsync();
+		}
+
 		public async Task RunAsync() {
 			_logger.LogInformation(nameof(RunAsync));
-
 			// 参考
 			// https://docs.microsoft.com/ja-jp/azure/storage/blobs/storage-quickstart-blobs-dotnet
 
 			var serviceClient = new BlobServiceClient(_connectionString);
 
 			var containerClient = serviceClient.GetBlobContainerClient("sample");
+
 			// todo:
 			var container = await containerClient.CreateIfNotExistsAsync();
 
-			// BLOB一覧を取得
+			// Blobをアップロード
+			await UpoladBlobAsync(containerClient, "a.txt", "Aaa");
+			await UpoladBlobAsync(containerClient, "b.txt", "Bbb");
+
+			// Blob一覧
 			await foreach (var item in containerClient.GetBlobsAsync()) {
 				_logger.LogInformation(item.Name);
 			}
+
+			// Blobをダウンロード
+			_logger.LogInformation(await DownloadBlobAsync(containerClient, "a.txt"));
+			_logger.LogInformation(await DownloadBlobAsync(containerClient, "b.txt"));
 		}
 	}
 }
