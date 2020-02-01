c162c25f1cf1e8a49e1d974c9ea8905527a62b7b
ichiroku11 <toolbox0x10@gmail.com>
2020-02-01 10:10:39 +0900

    LoggingConsoleApp: Refactor

--- src/EfCore3x/LoggingConsoleApp/Program.cs
+++ src/EfCore3x/LoggingConsoleApp/Program.cs
@@ -1,13 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace LoggingConsoleApp {
 	class Program {
-		static void Main(string[] args) {
+		private static async Task InitAsync() {
 			// 準備
-			using (var context = new AppDbContext()) {
-				var sql = @"
+			using var context = new AppDbContext();
+			var sql = @"
 drop table if exists dbo.Monster;
 
 create table dbo.Monster(
@@ -21,25 +22,30 @@ output inserted.*
 values
 	(1, N'スライム'),
 	(2, N'ドラキー');";
-				context.Database.ExecuteSqlRaw(sql);
-			}
+			await context.Database.ExecuteSqlRawAsync(sql);
+		}
 
-			using (var context = new AppDbContext()) {
-				// データを取得
-				var monsters = context.Monsters.ToList();
-				foreach (var monster in monsters) {
-					Console.WriteLine($"#{monster.Id} {monster.Name}");
-				}
-				//#1 スライム
-				//#2 ドラキー
-
-				/*
-				var monsters = context.Monsters.FromSqlRaw("select * from dbo.Monster;");
-				foreach (var monster in monsters) {
-					Console.WriteLine($"#{monster.Id} {monster.Name}");
-				}
-				*/
+		private static async Task RunAsync() {
+			using var context = new AppDbContext();
+			// データを取得
+			var monsters = await context.Monsters.ToListAsync();
+			foreach (var monster in monsters) {
+				Console.WriteLine($"#{monster.Id} {monster.Name}");
 			}
+			//#1 スライム
+			//#2 ドラキー
+
+			/*
+			var monsters = context.Monsters.FromSqlRaw("select * from dbo.Monster;");
+			foreach (var monster in monsters) {
+				Console.WriteLine($"#{monster.Id} {monster.Name}");
+			}
+			*/
+		}
+
+		static async Task Main(string[] args) {
+			await InitAsync();
+			await RunAsync();
 		}
 	}
 }
