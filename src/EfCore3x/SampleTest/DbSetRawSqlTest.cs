813886c333dbbe20f8ecc4d5b5f17f53bd4ab5fd
ichiroku11 <toolbox0x10@gmail.com>
2020-02-02 14:54:10 +0900

    SampleTest: Update DbSetRawSqlTest

--- src/EfCore3x/SampleTest/DbSetRawSqlTest.cs
+++ src/EfCore3x/SampleTest/DbSetRawSqlTest.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace SampleTest {
 			var samples = await context.Samples
 				// パラメータを使わないとあまり補間の意味が無いかも
 				.FromSqlInterpolated($"select 1 as Id, N'a' as Name")
+				// ネストされたSQL文にならないように
 				// ここでSQLを実行する
 				.ToListAsync();
 			var sample = samples.First();
@@ -22,11 +24,61 @@ namespace SampleTest {
 		}
 
 		[Fact]
+		public async Task FromSqlInterpolated_パラメータを使ったクエリその1() {
+			using var context = new AppDbContext();
+
+			var samples = await context.Samples
+				.FromSqlInterpolated($"select {1} as Id, {"a"} as Name")
+				.ToListAsync();
+			// 実行されるSQL
+			// select @p0 as Id, @p1 as Name
+			var sample = samples.First();
+
+			Assert.Equal(1, sample.Id);
+			Assert.Equal("a", sample.Name);
+		}
+
+		// その1と同じ
+		[Fact]
+		public async Task FromSqlInterpolated_パラメータを使ったクエリその2() {
+			using var context = new AppDbContext();
+
+			// 同じ
+			//var param = (id: 1, name: "a");
+			var param = new { id = 1, name = "a" };
+			var samples = await context.Samples
+				.FromSqlInterpolated($"select {param.id} as Id, {param.name} as Name")
+				.ToListAsync();
+			// 実行されるSQL
+			// select @p0 as Id, @p1 as Name
+			var sample = samples.First();
+
+			Assert.Equal(1, sample.Id);
+			Assert.Equal("a", sample.Name);
+		}
+
+
+		[Fact]
 		public async Task FromSqlRaw_パラメータを使わないクエリ() {
 			using var context = new AppDbContext();
 
 			var samples = await context.Samples
 				.FromSqlRaw("select 1 as Id, N'a' as Name")
+				.ToListAsync();
+			var sample = samples.First();
+
+			Assert.Equal(1, sample.Id);
+			Assert.Equal("a", sample.Name);
+		}
+
+		[Fact]
+		public async Task FromSqlRaw_パラメータを使ったクエリ() {
+			using var context = new AppDbContext();
+
+			var samples = await context.Samples
+				.FromSqlRaw(
+					"select @id as Id, @name as Name",
+					new SqlParameter("id", 1), new SqlParameter("name", "a"))
 				.ToListAsync();
 			var sample = samples.First();
 
