0ba6e296f49d8ac852d4f1c4cb32d2d3ad4cf2fd
ichiroku11 <toolbox0x10@gmail.com>
2020-02-02 12:50:22 +0900

    SampleTest: FromSqlInterpolated

--- src/EfCore3x/SampleTest/DbSetRawSqlTest.cs
+++ src/EfCore3x/SampleTest/DbSetRawSqlTest.cs
@@ -7,13 +7,26 @@ using Xunit;
 namespace SampleTest {
 	public class DbSetRawSqlTest {
 		[Fact]
+		public async Task FromSqlInterpolated_パラメータを使わないクエリ() {
+			using var context = new AppDbContext();
+
+			var samples = await context.Samples
+				// パラメータを使わないとあまり補間の意味が無いかも
+				.FromSqlInterpolated($"select 1 as Id, N'a' as Name")
+				// ここでSQLを実行する
+				.ToListAsync();
+			var sample = samples.First();
+
+			Assert.Equal(1, sample.Id);
+			Assert.Equal("a", sample.Name);
+		}
+
+		[Fact]
 		public async Task FromSqlRaw_パラメータを使わないクエリ() {
 			using var context = new AppDbContext();
 
-			var samples = await context
-				.Samples
+			var samples = await context.Samples
 				.FromSqlRaw("select 1 as Id, N'a' as Name")
-				// ここでSQLを実行する
 				.ToListAsync();
 			var sample = samples.First();
 
