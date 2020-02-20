78f5f576a12fb45346955827572c9a0cd1682083
ichiroku11 <toolbox0x10@gmail.com>
2020-02-20 23:01:08 +0900

    SampleTest: XUnitTest

--- src/DotNetCore3x/SampleTest/XUnitTest.cs
+++ src/DotNetCore3x/SampleTest/XUnitTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 using Xunit.Abstractions;
@@ -38,6 +39,40 @@ namespace SampleTest {
 		public void Theory属性とInlineData属性でパラメータを使ったテスト(int value) {
 			Assert.Equal(0, value % 2);
 		}
+
+		// パラメータを生成するプロパティ
+		public static IEnumerable<object[]> EvenNumbers {
+			get {
+				yield return new object[] { 2 };
+				yield return new object[] { 4 };
+			}
+		}
+
+		[Theory]
+		// プロパティからテストデータを取得する
+		[MemberData(nameof(EvenNumbers))]
+		public void Theory属性とMemberData属性でプロパティからテストデータを生成したテスト(int value) {
+			_output.WriteLine($"{value}");
+
+			Assert.Equal(0, value % 2);
+		}
+
+		// パラメータを生成するメソッド
+		public static IEnumerable<object[]> GetEvenNumbers(int count) {
+			foreach (var index in Enumerable.Range(0, count)) {
+				yield return new object[] { 2 * index };
+			}
+		}
+
+		[Theory]
+		// メソッドからテストデータを取得する
+		[MemberData(nameof(GetEvenNumbers), 2)]
+		public void Theory属性とMemberData属性でメソッドからテストデータを生成したテスト(int value) {
+			_output.WriteLine($"{value}");
+
+			Assert.Equal(0, value % 2);
+		}
+
 
 	}
 }
