98fa3a519290f32ba82c188804e5169d489006c5
ichiroku11 <toolbox0x10@gmail.com>
2020-03-06 14:42:11 +0900

    PostRedirectGetWebApp: Add project

---
+++ src/AspNetCore3x/PostRedirectGetWebApp/Startup.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PostRedirectGetWebApp {
+	public class Startup {
+		public void ConfigureServices(IServiceCollection services) {
+			services.AddControllersWithViews();
+
+			services.Configure<RouteOptions>(options => {
+				options.LowercaseUrls = true;
+				options.LowercaseQueryStrings = true;
+			});
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints => {
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Default}/{action=Index}/{id?}");
+			});
+		}
+	}
+}
