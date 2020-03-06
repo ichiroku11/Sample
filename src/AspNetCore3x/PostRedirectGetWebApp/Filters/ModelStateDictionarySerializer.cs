2d5ccd0a6fb83e81a78304be33c20f0e4c7caca2
ichiroku11 <toolbox0x10@gmail.com>
2020-03-06 17:03:36 +0900

    PostRedirectGetWebApp: Update

---
+++ src/AspNetCore3x/PostRedirectGetWebApp/Filters/ModelStateDictionarySerializer.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostRedirectGetWebApp.Filters {
+	public static class ModelStateDictionarySerializer {
+		public static string Serialize(ModelStateDictionary modelState) {
+			// todo
+			throw new NotImplementedException();
+		}
+
+		public static ModelStateDictionary Deserialize(string json) {
+			// todo
+			throw new NotImplementedException();
+		}
+	}
+}
