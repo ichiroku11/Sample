8ec6ae5ddb4f3d6d9fb6e6d19f3c645f58e8d2ab
ichiroku11 <toolbox0x10@gmail.com>
2020-03-06 16:15:39 +0900

    TagHelperWebApp: Add form sample

--- src/AspNetCore3x/TagHelperWebApp/Controllerss/DefaultController.cs
+++ src/AspNetCore3x/TagHelperWebApp/Controllerss/DefaultController.cs
@@ -1,13 +1,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TagHelperWebApp.Models;
 
 namespace TagHelperWebApp.Controllerss {
 	public class DefaultController : Controller {
+		private readonly ILogger _logger;
+
+		public DefaultController(ILogger<DefaultController> logger) {
+			_logger = logger;
+		}
+
 		public IActionResult Index() {
 			return View();
+		}
+
+		public IActionResult Form() {
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Form(FormCommand command) {
+			foreach (var modelState in ModelState) {
+				var builder = new StringBuilder();
+				builder.Append("ModelState:")
+					.Append($" {nameof(modelState.Key)}={modelState.Key}")
+					.Append($", {nameof(modelState.Value.RawValue)}.Type={modelState.Value.RawValue.GetType()}")
+					.Append($", {nameof(modelState.Value.RawValue)}={modelState.Value.RawValue}");
+				_logger.LogInformation(builder.ToString());
+			}
+
+			return RedirectToAction(nameof(Form));
 		}
 	}
 }
---
+++ src/AspNetCore3x/TagHelperWebApp/Models/FormCommand.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelperWebApp.Models {
+	public class FormCommand {
+		public string Text { get; set; }
+
+		public bool CheckBox { get; set; }
+
+		public int Select { get; set; }
+
+		public string MultilineText { get; set; }
+	}
+}
