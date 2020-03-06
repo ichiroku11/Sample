08505bd7e947e0c833035ee4caee1f9b0efa54c4
ichiroku11 <toolbox0x10@gmail.com>
2020-03-06 22:49:42 +0900

    PostRedirectGetWebApp: Update ModelStateDictionaryJsonSerializerTest

--- src/AspNetCore3x/PostRedirectGetWebApp.Test/Filters/ModelStateDictionaryJsonSerializerTest.cs
+++ src/AspNetCore3x/PostRedirectGetWebApp.Test/Filters/ModelStateDictionaryJsonSerializerTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace PostRedirectGetWebApp.Filters {
@@ -32,6 +33,53 @@ namespace PostRedirectGetWebApp.Filters {
 			Assert.Equal(
 				@"[{""key"":""k"",""rawValues"":[""r1"",""r2""],""attemptedValue"":""a"",""errorMessages"":[]}]",
 				json);
+		}
+
+		[Fact]
+		public void Deserialize_RawValueがstringの場合に正しく処理できる() {
+			// Arrange
+			var json = @"[{""key"":""k"",""rawValues"":[""r""],""attemptedValue"":""a"",""errorMessages"":[]}]";
+
+			// Act
+			var modelStates = ModelStateDictionaryJsonSerializer.Deserialize(json);
+
+			// Assert
+			Assert.Single(modelStates);
+
+			var (key, modelState) = modelStates.First();
+			Assert.Equal("k", key);
+
+			Assert.IsType<string>(modelState.RawValue);
+			Assert.Equal("r", modelState.RawValue);
+
+			Assert.Equal("a", modelState.AttemptedValue);
+
+			Assert.Empty(modelState.Errors);
+		}
+
+		[Fact(DisplayName = "Deserialize_RawValueがstring[]の場合に正しく処理できる")]
+		public void Deserialize_RawValueStringArray() {
+			// Arrange
+			var json = @"[{""key"":""k"",""rawValues"":[""r1"",""r2""],""attemptedValue"":""a"",""errorMessages"":[]}]";
+
+			// Act
+			var modelStates = ModelStateDictionaryJsonSerializer.Deserialize(json);
+
+			// Assert
+			Assert.Single(modelStates);
+
+			var (key, modelState) = modelStates.First();
+			Assert.Equal("k", key);
+
+			Assert.IsType<string[]>(modelState.RawValue);
+			var rawValues = modelState.RawValue as string[];
+			Assert.Equal(2, rawValues.Length);
+			Assert.Equal("r1", rawValues[0]);
+			Assert.Equal("r2", rawValues[1]);
+
+			Assert.Equal("a", modelState.AttemptedValue);
+
+			Assert.Empty(modelState.Errors);
 		}
 	}
 }
