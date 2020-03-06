4bde6b5c33bafdad5e4a0520f3693f77df5f270c
ichiroku11 <toolbox0x10@gmail.com>
2020-03-06 17:48:07 +0900

    PostRedirectGetWebApp: Update ModelStateDictionarySerializer

--- src/AspNetCore3x/PostRedirectGetWebApp/Filters/ModelStateDictionarySerializer.cs
+++ src/AspNetCore3x/PostRedirectGetWebApp/Filters/ModelStateDictionarySerializer.cs
@@ -2,18 +2,46 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PostRedirectGetWebApp.Filters {
 	public static class ModelStateDictionarySerializer {
-		public static string Serialize(ModelStateDictionary modelState) {
-			// todo
-			throw new NotImplementedException();
+		// JSONシリアライズ用のオブジェクト
+		private class JsonEntry {
+			public string Key { get; set; }
+			public object RawValue { get; set; }
+			public string AttemptedValue { get; set; }
+			public IEnumerable<string> ErrorMessages { get; set; }
 		}
 
+		// JSON文字列にシリアライズ
+		public static string Serialize(ModelStateDictionary modelStates) {
+			var entries = modelStates.Select(entry => new JsonEntry {
+				Key = entry.Key,
+				RawValue = entry.Value.RawValue,
+				AttemptedValue = entry.Value.AttemptedValue,
+				ErrorMessages = entry.Value.Errors.Select(error => error.ErrorMessage),
+			});
+
+			return JsonSerializer.Serialize(entries);
+		}
+
+		// JSON文字列をデシリアライズ
 		public static ModelStateDictionary Deserialize(string json) {
-			// todo
-			throw new NotImplementedException();
+			var modelStates = new ModelStateDictionary();
+
+			var entries = JsonSerializer.Deserialize<JsonEntry[]>(json);
+			foreach (var entry in entries) {
+				// todo
+				// rawValueがダメ
+				modelStates.SetModelValue(entry.Key, entry.RawValue, entry.AttemptedValue);
+				foreach (var errorMessage in entry.ErrorMessages) {
+					modelStates.AddModelError(entry.Key, errorMessage);
+				}
+			}
+
+			return modelStates;
 		}
 	}
 }
