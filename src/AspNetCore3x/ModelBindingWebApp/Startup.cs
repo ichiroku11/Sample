f1ebed8666238f00895a46b4595d2eaed15c5de9
ichiroku11 <toolbox0x10@gmail.com>
2020-03-09 21:45:23 +0900

    PostRedirectGetWebApp => ModelBindingWebApp

--- src/AspNetCore3x/ModelBindingWebApp/Startup.cs
+++ src/AspNetCore3x/ModelBindingWebApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,11 @@ namespace ModelBindingWebApp {
 	public class Startup {
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddControllersWithViews();
+
+			services.Configure<RouteOptions>(options => {
+				options.LowercaseUrls = true;
+				options.LowercaseQueryStrings = true;
+			});
 		}
 
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
