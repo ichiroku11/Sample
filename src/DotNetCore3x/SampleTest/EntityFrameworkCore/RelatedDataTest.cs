d29e0188c7345befa6798ede5c7bb32d1710ec48
ichiroku11 <toolbox0x10@gmail.com>
2020-03-15 15:25:04 +0900

    SampleTest: Update RelatedDataTest

--- src/DotNetCore3x/SampleTest/EntityFrameworkCore/RelatedDataTest.cs
+++ src/DotNetCore3x/SampleTest/EntityFrameworkCore/RelatedDataTest.cs
@@ -6,6 +6,30 @@ namespace SampleTest.EntityFrameworkCore {
 	// 参考
 	// https://docs.microsoft.com/ja-jp/ef/core/querying/related-data
 	public class RelatedDataTest : IDisposable {
+		private class MonsterCategory {
+			public int Id { get; set; }
+			public string Name { get; set; }
+		}
+
+		private class Monster {
+			public int Id { get; set; }
+			public int CategoryId { get; set; }
+			public string Name { get; set; }
+		}
+
+		// todo:
+		/*
+		private class Item {
+			public int Id { get; set; }
+			public string Name { get; set; }
+		}
+
+		private class MonsterItem {
+			public int MonsterId { get; set; }
+			public int ItemId { get; set; }
+		}
+		*/
+
 		private class MonsterDbContext : AppDbContext {
 			// todo:
 		}
