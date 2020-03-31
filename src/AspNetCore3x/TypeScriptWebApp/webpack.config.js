ef22802eeb48e591b62cecbb6d06e96c1ff5bade
ichiroku11 <toolbox0x10@gmail.com>
2020-03-31 15:27:12 +0900

    TypeScriptWebApp: Add GameOfLife

--- src/AspNetCore3x/TypeScriptWebApp/webpack.config.js
+++ src/AspNetCore3x/TypeScriptWebApp/webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const config = {
 	entry: {
 		index: path.resolve(__dirname, "scripts/index.ts"),
+		"game/gameoflife": path.resolve(__dirname, "scripts/game/gameoflife.ts"),
 		"vuejs/usewebpack": path.resolve(__dirname, "scripts/vuejs/usewebpack.ts")
 	},
 	output: {
