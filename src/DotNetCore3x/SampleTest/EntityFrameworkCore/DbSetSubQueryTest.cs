9c5a7cec8a0f9b8e3fedf5e8c7bc7bc6c7ca17a1
ichiroku11 <toolbox0x10@gmail.com>
2020-04-07 21:06:33 +0900

    EntityFrameworkCore: Update DbSetSubQueryTest 相関サブクエリ

--- src/DotNetCore3x/SampleTest/EntityFrameworkCore/DbSetSubQueryTest.cs
+++ src/DotNetCore3x/SampleTest/EntityFrameworkCore/DbSetSubQueryTest.cs
@@ -11,6 +11,7 @@ namespace SampleTest.EntityFrameworkCore {
 		private class MenuItem {
 			public int Id { get; set; }
 			public string Name { get; set; }
+			public string Category { get; set; }
 			public decimal Price { get; set; }
 		}
 
@@ -44,18 +45,21 @@ namespace SampleTest.EntityFrameworkCore {
 			var sql = @"
 create table dbo.MenuItem(
 	Id int not null,
-	Name nvarchar(5) not null,
-	Price decimal(5) not null,
+	Name nvarchar(6) not null,
+	Category nvarchar(3) not null,
+	Price decimal(3) not null,
 	constraint PK_MenuItem primary key(Id)
 );
 
-insert into dbo.MenuItem(Id, Name, Price)
+insert into dbo.MenuItem(Id, Name, Category, Price)
 output inserted.*
 values
-	(1, N'けい', 500),
-	(2, N'かわ', 300),
-	(3, N'ねぎま', 300),
-	(4, N'しろ', 400);";
+	(1, N'純けい', N'串焼き', 500),
+	(2, N'しろ', N'串焼き', 400),
+	(3, N'若皮', N'串焼き', 300),
+	(4, N'串カツ', N'揚げ物', 400),
+	(5, N'ポテトフライ', N'揚げ物', 200),
+	(6, N'レンコン揚げ', N'揚げ物', 300);";
 			_context.Database.ExecuteSqlRaw(sql);
 		}
 
@@ -70,18 +74,50 @@ values
 				// 平均Price以上
 				.Where(item => item.Price >= _context.MenuItems.Average(item => item.Price))
 				.ToListAsync();
-			// 実行されるクエリ
+
+			// 実行されるSQL
 			/*
-			SELECT [m].[Id], [m].[Name], [m].[Price]
+			SELECT [m].[Id], [m].[Category], [m].[Name], [m].[Price]
 			FROM [MenuItem] AS [m]
 			WHERE [m].[Price] >= (
 				SELECT AVG([m0].[Price])
 				FROM [MenuItem] AS [m0])
 			*/
 
-			Assert.Equal(2, items.Count());
-			Assert.Contains(items, item => string.Equals(item.Name, "けい"));
+			Assert.Equal(3, items.Count());
+			Assert.Contains(items, item => string.Equals(item.Name, "純けい"));
 			Assert.Contains(items, item => string.Equals(item.Name, "しろ"));
+			Assert.Contains(items, item => string.Equals(item.Name, "串カツ"));
+		}
+
+		[Fact]
+		public async Task 相関サブクエリでカテゴリ別の平均Price以上のMenuItemを取得() {
+			var items = await _context.MenuItems
+				// カテゴリごとの平均Price以上
+				.Where(item1 => item1.Price >=
+					// カテゴリごとの平均Price
+					_context.MenuItems
+						// 相関
+						.Where(item2 => string.Equals(item2.Category, item1.Category))
+						//.Where(item2 => item2.Category == item1.Category)
+						.Average(item => item.Price))
+				.ToListAsync();
+
+			// 実行されるSQL
+			/*
+			SELECT [m].[Id], [m].[Category], [m].[Name], [m].[Price]
+			FROM [MenuItem] AS [m]
+			WHERE [m].[Price] >= (
+				SELECT AVG([m0].[Price])
+				FROM [MenuItem] AS [m0]
+				WHERE ([m0].[Category] = [m].[Category]) OR ([m0].[Category] IS NULL AND [m].[Category] IS NULL))
+			*/
+
+			Assert.Equal(4, items.Count());
+			Assert.Contains(items, item => string.Equals(item.Name, "純けい"));
+			Assert.Contains(items, item => string.Equals(item.Name, "しろ"));
+			Assert.Contains(items, item => string.Equals(item.Name, "串カツ"));
+			Assert.Contains(items, item => string.Equals(item.Name, "レンコン揚げ"));
 		}
 	}
 }
