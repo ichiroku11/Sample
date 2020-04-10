c0146bfad538dc6510ef36188e8fe4af4ba5a5ca
ichiroku11 <toolbox0x10@gmail.com>
2020-04-10 16:25:33 +0900

    SampleTest: Test Reflection

--- src/DotNetCore3x/SampleTest/Reflection/TypeInfoTest.cs
+++ src/DotNetCore3x/SampleTest/Reflection/TypeInfoTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -7,19 +8,22 @@ using Xunit;
 
 namespace SampleTest.Reflection {
 	public class TypeInfoTest {
-
 		public class Sample {
 			public int Value { get; set; }
 		}
 
 		[Fact]
-		public void GetCustomAttributes_CompilerGeneratedAttribute() {
+		public void GetterSetterプロパティは対応するメソッドが自動生成される() {
 			// Arrange
 			// Act
-			var atributes = typeof(Sample).GetTypeInfo()
-				.GetCustomAttributes<CompilerGeneratedAttribute>();
+			var methods = typeof(Sample).GetTypeInfo()
+				.GetMethods()
+				.Where(method => method.GetCustomAttributes<CompilerGeneratedAttribute>().Any());
 
 			// Assert
+			Assert.Equal(2, methods.Count());
+			Assert.Contains(methods, method => string.Equals(method.Name, $"get_{nameof(Sample.Value)}"));
+			Assert.Contains(methods, method => string.Equals(method.Name, $"set_{nameof(Sample.Value)}"));
 		}
 	}
 }
