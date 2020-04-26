16defc4d9e2e916891ac7d43f3e9024ce3c0ba2c
ichiroku11 <toolbox0x10@gmail.com>
2020-04-26 11:34:38 +0900

    TypeScriptWebApp: Add lib.ts

--- src/AspNetCore3x/TypeScriptWebApp/webpack.config.js
+++ src/AspNetCore3x/TypeScriptWebApp/webpack.config.js
@@ -3,11 +3,20 @@ const path = require("path");
 
 const config = {
 	entry: {
-		index: path.resolve(__dirname, "scripts/index.ts"),
+		// todo:
+		"lib": path.resolve(__dirname, "scripts/lib.ts"),
+		"index": path.resolve(__dirname, "scripts/index.ts"),
 		"importmodule": path.resolve(__dirname, "scripts/importmodule.ts"),
 		"game/gameoflife": path.resolve(__dirname, "scripts/game/gameoflife.ts"),
 		"vuejs/usewebpack": path.resolve(__dirname, "scripts/vuejs/usewebpack.ts")
 	},
+	// todo:
+	/*
+	optimization: {
+		splitChunks: {
+		}
+	},
+	*/
 	output: {
 		filename: "[name].js",
 		path: path.resolve(__dirname, "wwwroot/js")
