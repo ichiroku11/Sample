9a003216c968de27c05bf91d6d8dfba6c79eb4a1
ichiroku11 <toolbox0x10@gmail.com>
2020-05-10 08:50:22 +0900

    TypeScriptWebApp: update spread syntax test

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/test/spread-syntax.test.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/test/spread-syntax.test.ts
@@ -3,8 +3,10 @@ import { Assert, Test } from "../unittestlib";
 // スプレッド構文
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 
+const sum = (...values: number[]): number => values.reduce((previous, current) => previous + current);
+
 export const spreadSyntaxTest = new Test("SpreadSyntaxTest")
-	.fact("spread_配列で使ってみる", () => {
+	.fact("spread_配列リテラルで使ってみる", () => {
 		// Arrange
 		const items = [1, 2];
 		// Act
@@ -13,7 +15,7 @@ export const spreadSyntaxTest = new Test("SpreadSyntaxTest")
 		// Assert
 		Assert.equal([0, 1, 2, 3], actual);
 	})
-	.fact("spread_配列を連結する", () => {
+	.fact("spread_配列リテラルで配列を連結する", () => {
 		// Arrange
 		const items1 = [0, 1];
 		const items2 = [2, 3];
@@ -22,4 +24,21 @@ export const spreadSyntaxTest = new Test("SpreadSyntaxTest")
 
 		// Assert
 		Assert.equal([0, 1, 2, 3], actual);
+	})
+	.fact("spread_関数の可変長配列の引数で使ってみる", () => {
+		// Arrange
+		// Act
+		const actual = sum(1, 2, 3);
+
+		// Assert
+		Assert.equal(6, actual);
+	})
+	.fact("spread_関数の可変長配列の引数で使ってみるその2", () => {
+		// Arrange
+		// Act
+		const values = [1, 2, 3];
+		const actual = sum(...values);
+
+		// Assert
+		Assert.equal(6, actual);
 	});
