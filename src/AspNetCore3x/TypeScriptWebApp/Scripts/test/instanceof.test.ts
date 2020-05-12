8d2180e09da322ff04fb991a486e9520ee6a9adc
ichiroku11 <toolbox0x10@gmail.com>
2020-05-12 19:53:52 +0900

    TypeScriptWebApp: test instanceof

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/test/instanceof.test.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/test/instanceof.test.ts
@@ -8,9 +8,22 @@ class Bird extends Animal {}
 class Crow extends Bird {}
 
 export const instanceofTest = new Test("InstanceOfTest")
-	.fact("instanceof_試す", async () => {
+	.fact("instanceof_特定のクラスのインスタンスかどうか判定できる", async () => {
 		// Arrange
 		// Act
 		// Asseert
-		// todo:
+
+		// instanceofは継承も考慮して特定のクラスに属しているか（特定のクラスのサブクラスか）を判定できる
+
+		const crow = new Crow();
+		// crowは
+		Assert.true(crow instanceof Crow);	// Crowのインスタンスである
+		Assert.true(crow instanceof Bird);	// Birdのサブクラスのインスタンスである
+		Assert.true(crow instanceof Animal);	// Animalのサブクラスのインスタンスである
+		Assert.true(crow instanceof Object);	// Objectのサブクラスのインスタンスである
+
+		const bird = new Bird();
+		// birdは
+		Assert.false(bird instanceof Crow);	// Crowのインスタンスではない
+		Assert.true(bird instanceof Bird);	// Birdのサブクラスのインスタンスである
 	});
