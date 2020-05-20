2ec73bf2ddf054385350124554edc2a8818a2541
ichiroku11 <toolbox0x10@gmail.com>
2020-05-20 22:59:52 +0900

    SassWebApp: Update grid layout sample

---
+++ src/AspNetCore3x/SassWebApp/Pages/GridLayout.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SassWebApp.Pages {
+	public class GridLayoutModel : PageModel {
+		[BindProperty(SupportsGet = true)]
+		public int Pattern { get; set; }
+
+		public void OnGet() {
+		}
+	}
+}
