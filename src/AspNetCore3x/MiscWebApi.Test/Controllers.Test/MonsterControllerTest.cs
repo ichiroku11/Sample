b059d768a3ab6987c940910df62f700c289bfb01
ichiroku11 <toolbox0x10@gmail.com>
2020-06-02 22:51:28 +0900

    MiscWebApi.Test: Create test

---
+++ src/AspNetCore3x/MiscWebApi.Test/Controllers.Test/MonsterControllerTest.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using MiscWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MiscWebApi.Test.Controllers.Test {
+	public class MonsterControllerTest : IClassFixture<WebApplicationFactory<Startup>> {
+		private static readonly JsonSerializerOptions _jsonSerializerOptions
+			= new JsonSerializerOptions {
+				PropertyNameCaseInsensitive = true,
+			};
+
+		private readonly WebApplicationFactory<Startup> _factory;
+
+		public MonsterControllerTest(WebApplicationFactory<Startup> factory) {
+			_factory = factory;
+		}
+
+		[Fact]
+		public async Task Get() {
+			// Arrange
+			using var client = _factory.CreateClient();
+
+			// Act
+			using var response = await client.GetAsync("/api/monster");
+			var json = await response.Content.ReadAsStringAsync();
+
+			var monsters = JsonSerializer.Deserialize<IList<Monster>>(json, _jsonSerializerOptions);
+
+			// Assert
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+			Assert.Equal(2, monsters.Count);
+			Assert.Contains(monsters, monster => monster.Id == 1 && monster.Name == "スライム");
+			Assert.Contains(monsters, monster => monster.Id == 2 && monster.Name == "ドラキー");
+		}
+	}
+}
