7efafb65b8ca2e2cd11a735d00cf0d507977fac5
ichiroku11 <toolbox0x10@gmail.com>
2020-06-13 23:03:22 +0900

    SampleTest: Test TypeConverter

--- src/DotNetCore3x/SampleTest/TypeConverterTest.cs
+++ src/DotNetCore3x/SampleTest/TypeConverterTest.cs
@@ -8,6 +8,8 @@ using System.Text;
 using Xunit;
 
 namespace SampleTest {
+	// TypeConverter クラス (System.ComponentModel) | Microsoft Docs
+	// https://docs.microsoft.com/ja-jp/dotnet/api/system.componentmodel.typeconverter?view=netcore-3.1
 	public class TypeConverterTest {
 		public enum Fruit {
 			None = 0,
@@ -22,15 +24,21 @@ namespace SampleTest {
 		[Theory]
 		[MemberData(nameof(GetFruits))]
 		public void IsValid_enum値が定義されていることを判定できる(Fruit fruit) {
+			// Arrange
 			var converter = TypeDescriptor.GetConverter(typeof(Fruit));
 
+			// Act
+			// Assert
 			Assert.True(converter.IsValid(fruit));
 		}
 
 		[Fact]
 		public void IsValid_enum値が定義されている数値はtrueになる() {
+			// Arrange
 			var converter = TypeDescriptor.GetConverter(typeof(Fruit));
 
+			// Act
+			// Assert
 			Assert.True(converter.IsValid(1));
 		}
 
@@ -38,9 +46,38 @@ namespace SampleTest {
 		[InlineData((int)Fruit.None - 1)]
 		[InlineData((int)Fruit.Banana + 1)]
 		public void IsValid_enum値が定義されていない数値はfalseになる(int nofruit) {
+			// Arrange
 			var converter = TypeDescriptor.GetConverter(typeof(Fruit));
 
+			// Act
+			// Assert
 			Assert.False(converter.IsValid(nofruit));
+		}
+
+		[Fact]
+		public void ConvertToString_Enumを文字列に変換できる() {
+			// Arrange
+			var converter = TypeDescriptor.GetConverter(typeof(Fruit));
+
+			// Act
+			var actual = converter.ConvertToString(Fruit.Apple);
+
+			// Assert
+			Assert.Equal("Apple", actual, StringComparer.OrdinalIgnoreCase);
+		}
+
+		[Theory]
+		[InlineData("Apple")]
+		[InlineData("apple")]
+		public void ConvertFromString_文字列からEnumに変換できる(string value) {
+			// Arrange
+			var converter = TypeDescriptor.GetConverter(typeof(Fruit));
+
+			// Act
+			var actual = (Fruit)converter.ConvertFromString(value);
+
+			// Assert
+			Assert.Equal(Fruit.Apple, actual);
 		}
 	}
 }
