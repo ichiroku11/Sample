2e09f7f6ac671b58c4eed3d49351b7cdd3758dd9
ichiroku11 <toolbox0x10@gmail.com>
2020-06-23 23:19:55 +0900

    TypeScriptWebApp: Update SVG clock

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/misc/svgclock.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/misc/svgclock.ts
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", event => {
-	console.log("SVG Clock");
+// 参考
+// SVG エッセンシャルズ 第2版
+// https://www.oreilly.co.jp/books/9784873117973/
+
+const clock = document.querySelector<SVGSVGElement>("#clock");
+const namespace = "http://www.w3.org/2000/svg";
+
+clock?.addEventListener("load", _ => {
+	try {
+		clock.suspendRedraw(1000);
+
+		// 文字盤
+		const face = document.createElementNS(namespace, "circle");
+		face.cx.baseVal.value = 125;
+		face.cy.baseVal.value = 125;
+		face.r.baseVal.value = 100;
+		face.style.cssText = "fill: white; stroke: black;";
+		clock.appendChild(face);
+
+	} finally {
+		clock.unsuspendRedrawAll();
+	}
 });
