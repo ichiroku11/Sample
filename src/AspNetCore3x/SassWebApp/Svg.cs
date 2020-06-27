75b3386c68f11fa0d693515382cb89a6993f978a
ichiroku11 <toolbox0x10@gmail.com>
2020-06-27 21:51:56 +0900

    SassWebApp: Refactor

---
+++ src/AspNetCore3x/SassWebApp/Svg.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SassWebApp {
+	// SVG
+	public enum Svg {
+		// </>
+		CodeSlash = 1,
+	}
+}
