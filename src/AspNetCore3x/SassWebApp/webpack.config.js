4da3e9fc71c09795e5a28d4251e9f6589b1430a2
ichiroku11 <toolbox0x10@gmail.com>
2020-07-05 22:49:07 +0900

    SassWebApp: Add Media.cshtml

--- src/AspNetCore3x/SassWebApp/webpack.config.js
+++ src/AspNetCore3x/SassWebApp/webpack.config.js
@@ -15,6 +15,7 @@ const config = {
 		gridlayout: path.resolve(__dirname, "styles/gridlayout.scss"),
 		gridrepeat: path.resolve(__dirname, "styles/gridrepeat.scss"),
 		index: path.resolve(__dirname, "styles/index.scss"),
+		media: path.resolve(__dirname, "styles/media.scss"),
 		transform: path.resolve(__dirname, "styles/transform.scss"),
 		transition: path.resolve(__dirname, "styles/transition.scss")
 	},
