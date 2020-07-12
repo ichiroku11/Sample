d85da052a6cdd54823bb33b7c691e7c9d933a902
ichiroku11 <toolbox0x10@gmail.com>
2020-07-12 09:51:55 +0900

    TypeScriptWebApp: Update fetch test

--- src/AspNetCore3x/TypeScriptWebApp/Controllers/SampleJsonController.cs
+++ src/AspNetCore3x/TypeScriptWebApp/Controllers/SampleJsonController.cs
@@ -6,14 +6,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TypeScriptWebApp.Controllers {
+	public class Sample {
+		public int Number { get; set; }
+		public string Text { get; set; }
+	}
+
 	[Route("api/[controller]")]
 	[ApiController]
 	public class SampleJsonController : ControllerBase {
 		public object Get() {
-			return new {
+			return new Sample {
 				Number = 1,
 				Text = "a",
 			};
+		}
+
+		[HttpPost]
+		public object Post(Sample sample) {
+			return sample;
 		}
 	}
 }
--- src/AspNetCore3x/TypeScriptWebApp/Scripts/test/fetch.test.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/test/fetch.test.ts
@@ -4,15 +4,32 @@ import { Assert, Test } from "../unittestlib";
 // https://developer.mozilla.org/ja/docs/Web/API/Fetch_API
 
 const baseUrl = "/api/samplejson";
-type GetResponse = { number: number, text: string };
+type Sample = { number: number, text: string };
 
 export const fetchTest = new Test("FetchTest")
 	.fact("GETリクエストのサンプル", async () => {
+		// GETリクエスト
 		const response = await fetch(baseUrl);
 		Assert.equal(200, response.status);
 
 		// レスポンスからJSONを取り出す
-		const json: GetResponse = await response.json();
+		const json: Sample = await response.json();
 		Assert.equal(json.number, 1);
 		Assert.equal(json.text, "a");
-	});
+	})
+	.fact("POSTリクエスト（JSON）のサンプル", async () => {
+		// POSTリクエスト
+		const response = await fetch(baseUrl, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ number: 2, text: "b" }),
+		});
+		Assert.equal(200, response.status);
+
+		// レスポンスからJSONを取り出す
+		const json: Sample = await response.json();
+		Assert.equal(json.number, 2);
+		Assert.equal(json.text, "b");
+	})
