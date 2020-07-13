b24c648fadd9f8a1d1312fe0e98e9084169d3895
ichiroku11 <toolbox0x10@gmail.com>
2020-07-13 19:47:23 +0900

    TypeScriptWebApp: Update URLSearchParams test

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/test/url.test.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/test/url.test.ts
@@ -10,4 +10,17 @@ export const urlSearchParamsTest = new Test("URLSearchParamsTest")
 		// Act
 		// Assert
 		Assert.equal("x=1&y=2", params.toString());
+	})
+	.fact("get_appendした値を取得できる", () => {
+		// Arrange
+		const params = new URLSearchParams();
+		params.append("x", "1");
+		params.append("y", "2");
+
+		// Act
+		// Assert
+		Assert.equal("1", params.get("x"));
+		Assert.equal("2", params.get("y"));
+		// todo:
+		//Assert.equal(null, params.get("z"));
 	});
