ad825a587c8d14fee42a68d74e5f9649cbacb983
ichiroku11 <toolbox0x10@gmail.com>
2020-07-23 10:08:07 +0900

    SampleTest: Test UrlHelper.ActionLink

--- src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
+++ src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -10,6 +11,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SampleTest.AspNetCore {
 	// 参考
@@ -73,6 +75,33 @@ namespace SampleTest.AspNetCore {
 			return new RouteBuilder(app.Object) {
 				DefaultHandler = new PassThroughRouter(),
 			};
+		}
+
+		// IRouterを生成
+		private static IRouter CreateRouter(IServiceProvider services) {
+			var routeBuilder = CreateRouteBuilder(services);
+			// デフォルトのルートを追加
+			routeBuilder.MapRoute(
+				name: "default",
+				template: "{controller}/{action}/{id?}",
+				new { controller = "default", action = "index" });
+			return routeBuilder.Build();
+		}
+
+		[Fact]
+		public void ActionLink_絶対URLを生成できる() {
+			// Arrange
+			var services = CreateServiceProvider();
+			var httpContext = CreateHttpContext(services);
+			var actionContext = CreateActionContext(httpContext);
+			actionContext.RouteData.Routers.Add(CreateRouter(services));
+			var urlHelper = new UrlHelper(actionContext);
+
+			// Act
+			var url = urlHelper.ActionLink();
+
+			// Assert
+			Assert.Equal("http://localhost/", url);
 		}
 	}
 }
