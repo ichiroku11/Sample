5bd4f83c10e3dfc3dc1afbc0cca0b71369a90e0d
ichiroku11 <toolbox0x10@gmail.com>
2020-07-23 09:58:49 +0900

    SampleTest: Update UrlHelperTest

--- src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
+++ src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace SampleTest.AspNetCore {
 	public class UrlHelperTest {
+		// 参考
+		// https://github.com/dotnet/aspnetcore/blob/master/src/Mvc/Mvc.Core/test/Routing/UrlHelperTest.cs
+		// https://github.com/dotnet/aspnetcore/blob/master/src/Mvc/Mvc.Core/test/Routing/UrlHelperTestBase.cs
 		private class PassThroughRouter : IRouter {
 			public VirtualPathData GetVirtualPath(VirtualPathContext context) => null;
 
@@ -13,6 +18,21 @@ namespace SampleTest.AspNetCore {
 				routeContext.Handler = httpContext => Task.CompletedTask;
 				return Task.CompletedTask;
 			}
+		}
+
+		private static IServiceProvider GetServiceProvider() {
+			var services = new ServiceCollection();
+
+			services
+				.AddOptions()
+				.AddLogging()
+				.AddRouting(options => {
+					options.LowercaseQueryStrings = true;
+					options.LowercaseUrls = true;
+				})
+				.AddSingleton(UrlEncoder.Default);
+
+			return services.BuildServiceProvider();
 		}
 	}
 }
