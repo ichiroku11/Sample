202d219aa472455a2c063f8200b24203d7919c69
ichiroku11 <toolbox0x10@gmail.com>
2020-07-23 10:04:06 +0900

    SampleTest: Update UrlHelperTest

--- src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
+++ src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,10 +12,10 @@ using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace SampleTest.AspNetCore {
+	// 参考
+	// https://github.com/dotnet/aspnetcore/blob/master/src/Mvc/Mvc.Core/test/Routing/UrlHelperTest.cs
+	// https://github.com/dotnet/aspnetcore/blob/master/src/Mvc/Mvc.Core/test/Routing/UrlHelperTestBase.cs
 	public class UrlHelperTest {
-		// 参考
-		// https://github.com/dotnet/aspnetcore/blob/master/src/Mvc/Mvc.Core/test/Routing/UrlHelperTest.cs
-		// https://github.com/dotnet/aspnetcore/blob/master/src/Mvc/Mvc.Core/test/Routing/UrlHelperTestBase.cs
 		private class PassThroughRouter : IRouter {
 			public VirtualPathData GetVirtualPath(VirtualPathContext context) => null;
 
@@ -20,7 +25,8 @@ namespace SampleTest.AspNetCore {
 			}
 		}
 
-		private static IServiceProvider GetServiceProvider() {
+		// IServiceProvider（サービス一覧）を生成
+		private static IServiceProvider CreateServiceProvider() {
 			var services = new ServiceCollection();
 
 			services
@@ -33,6 +39,40 @@ namespace SampleTest.AspNetCore {
 				.AddSingleton(UrlEncoder.Default);
 
 			return services.BuildServiceProvider();
+		}
+
+		// HttpContextを生成
+		private static HttpContext CreateHttpContext(
+			IServiceProvider services,
+			string scheme = "http",
+			string host = "localhost",
+			string app = "") {
+			var context = new DefaultHttpContext {
+				RequestServices = services
+			};
+
+			var request = context.Request;
+			request.Scheme = scheme;
+			request.Host = new HostString(host);
+			request.PathBase = new PathString(app);
+
+			return context;
+		}
+
+		// ActionContextを生成
+		private static ActionContext CreateActionContext(HttpContext httpContext, RouteData routeData = default)
+			=> new ActionContext(httpContext, routeData ?? new RouteData(), new ActionDescriptor());
+
+		// IRouteBuilderを生成
+		private static IRouteBuilder CreateRouteBuilder(IServiceProvider services) {
+			var app = new Mock<IApplicationBuilder>();
+
+			app.SetupGet(a => a.ApplicationServices)
+				.Returns(services);
+
+			return new RouteBuilder(app.Object) {
+				DefaultHandler = new PassThroughRouter(),
+			};
 		}
 	}
 }
