63b49981e6f5a82584d7846fbf4cb2a454374cbe
ichiroku11 <toolbox0x10@gmail.com>
2020-07-23 09:50:35 +0900

    SampleTest: Add UrlHelperTest

---
+++ src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest.AspNetCore {
+	public class UrlHelperTest {
+	}
+}
