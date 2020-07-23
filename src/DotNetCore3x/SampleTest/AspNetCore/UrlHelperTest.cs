72a6507bba30fe0e34c2d060673c08b7f8b30ae7
ichiroku11 <toolbox0x10@gmail.com>
2020-07-23 09:50:56 +0900

    SampleTest: Update UrlHelperTest

--- src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
+++ src/DotNetCore3x/SampleTest/AspNetCore/UrlHelperTest.cs
@@ -6,5 +6,13 @@ using System.Threading.Tasks;
 
 namespace SampleTest.AspNetCore {
 	public class UrlHelperTest {
+		private class PassThroughRouter : IRouter {
+			public VirtualPathData GetVirtualPath(VirtualPathContext context) => null;
+
+			public Task RouteAsync(RouteContext routeContext) {
+				routeContext.Handler = httpContext => Task.CompletedTask;
+				return Task.CompletedTask;
+			}
+		}
 	}
 }
