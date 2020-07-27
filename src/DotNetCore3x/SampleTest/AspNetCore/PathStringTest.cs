b68f07df477402c28be6a1975b25b6bbb5aa0b67
ichiroku11 <toolbox0x10@gmail.com>
2020-07-27 15:44:15 +0900

    SampleTest: Test PathString

--- src/DotNetCore3x/SampleTest/AspNetCore/PathStringTest.cs
+++ src/DotNetCore3x/SampleTest/AspNetCore/PathStringTest.cs
@@ -25,5 +25,38 @@ namespace SampleTest.AspNetCore {
 				new PathString("app");
 			});
 		}
+
+		[Theory]
+		[InlineData("/", "/", true)]
+		[InlineData("/app/abc", "/app", true)]
+		[InlineData("/app/abc", "/app/abc", true)]
+		[InlineData("/app/abc", "/", false)]
+		[InlineData("/app/abc", "/app/", false)]
+		[InlineData("/app/abc", "/app/t", false)]
+		[InlineData("/app/abc/xyz", "/app", true)]
+		[InlineData("/app/abc/xyz", "/app/abc", true)]
+		[InlineData("/app/abc/xyz", "/app/abc/xyz", true)]
+		[InlineData("/app/abc/xyz", "/app/abc/", false)]
+		public void StartsWithSegments_インスタンスの先頭が指定されたPathStringと一致するかどうか(string src, string test, bool expected) {
+			// Arrange
+			// Act
+			var actual = new PathString(src).StartsWithSegments(new PathString(test));
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData("/app/test", "/app", true)]
+		[InlineData("/app/test", "/APP", true)]
+		[InlineData("/APP/test", "/app", true)]
+		public void StartsWithSegments_大文字小文字を区別しないで比較する(string src, string test, bool expected) {
+			// Arrange
+			// Act
+			var actual = new PathString(src).StartsWithSegments(new PathString(test));
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
 	}
 }
