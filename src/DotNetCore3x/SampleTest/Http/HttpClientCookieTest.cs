02aa48c4b8a361d681e3060d4d4f3f7deee4d774
ichiroku11 <toolbox0x10@gmail.com>
2020-08-10 14:15:41 +0900

    HttpClientCookieTest

--- src/DotNetCore3x/SampleTest/Http/HttpClientCookieTest.cs
+++ src/DotNetCore3x/SampleTest/Http/HttpClientCookieTest.cs
@@ -1,35 +1,33 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace SampleTest.Http {
-	public class HttpClientHandlerTest : IDisposable {
+	public class HttpClientCookieTest : IDisposable {
 		private class Startup {
 			public void ConfigureServices(IServiceCollection services) {
 			}
 
 			public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-				app.Run(async context => {
-					context.Response.ContentType = "text/plain";
+				app.Run(context => {
+					// Set-Cookieヘッダを付与
 					context.Response.Cookies.Append("abc", "xyz");
-					await context.Response.WriteAsync("Hello World!");
+					return Task.CompletedTask;
 				});
 			}
 		}
 
 		private readonly TestServer _server;
 
-		public HttpClientHandlerTest() {
+		public HttpClientCookieTest() {
 			_server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
 		}
 
@@ -38,24 +36,19 @@ namespace SampleTest.Http {
 		}
 
 		[Fact]
-		public async Task GetAsync_とりあえず使ってみる() {
+		public async Task GetAsync_レスポンスのSetCookieヘッダを取得する() {
 			// Arrange
-			using var handler = _server.CreateHandler();
-			using var client = new HttpClient(handler) {
-				BaseAddress = _server.BaseAddress,
-			};
-
+			using var client = _server.CreateClient();
 			// Act
 			var response = await client.GetAsync("/");
 
 			var statusCode = response.StatusCode;
-			var contentType = response.Content.Headers.ContentType.MediaType;
-			var contentText = await response.Content.ReadAsStringAsync();
+			var containsSetCookie = response.Headers.TryGetValues("Set-Cookie", out var cookieValues);
 
 			// Assert
 			Assert.Equal(HttpStatusCode.OK, statusCode);
-			Assert.Equal("text/plain", contentType);
-			Assert.Equal("Hello World!", contentText);
+			Assert.True(containsSetCookie);
+			Assert.Equal("abc=xyz; path=/", cookieValues.Single());
 		}
 	}
 }
