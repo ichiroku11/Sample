38ba755ba93cd3b554d83b445f40e7d4e6a6b5d9
ichiroku11 <toolbox0x10@gmail.com>
2020-08-18 15:35:30 +0900

    SampleTest: Test Observable.Subscribe

--- src/DotNetCore3x/SampleTest/Reactive/ObservableTest.cs
+++ src/DotNetCore3x/SampleTest/Reactive/ObservableTest.cs
@@ -26,6 +26,38 @@ namespace SampleTest.Reactive {
 		// Using
 
 		[Fact]
+		public void Subscribe_リターンされたIDisposableのDisposeメソッドはUnsubscribeという意味() {
+			// Arrange
+			var disposed = false;
+			var observable = Observable.Create<int>(observer => {
+				// Disposeが呼ばれたときの処理
+				return () => {
+					// ここでリソースの解放などをすると良いらしい
+					disposed = true;
+				};
+			});
+			var completed = false;
+
+			// Act
+			var disposable = observable.Subscribe(
+				onNext: _ => AssertHelper.Fail(),
+				onError: _ => AssertHelper.Fail(),
+				onCompleted: () => {
+					completed = true;
+				});
+
+			// Assert
+			Assert.False(disposed);
+			Assert.False(completed);
+
+			// IDisposable.DisposeはUnsubscribeの意味
+			disposable.Dispose();
+			Assert.True(disposed);
+			// onCompletedは呼ばれない
+			Assert.False(completed);
+		}
+
+		[Fact]
 		public void Create_任意のObservableを生成する() {
 			// Arrange
 			var disposed = false;
