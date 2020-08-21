79dd1ddc1815ffc280cb4f32fd7aa058fd8e911a
ichiroku11 <toolbox0x10@gmail.com>
2020-08-21 23:13:27 +0900

    SampleTest: Test Observable.Skip, Observable.Take

--- src/DotNetCore3x/SampleTest/Reactive/ObservableFilteringTest.cs
+++ src/DotNetCore3x/SampleTest/Reactive/ObservableFilteringTest.cs
@@ -26,5 +26,33 @@ namespace SampleTest.Reactive {
 			// Assert
 			Assert.Equal(new List<int> { 0, 2, 4 }, values);
 		}
+
+		[Fact]
+		public void Skip_試す() {
+			// Arrange
+			var values = new List<int>();
+
+			// Act
+			Observable.Range(0, 5)
+				.Skip(3)
+				.Subscribe(value => values.Add(value));
+
+			// Assert
+			Assert.Equal(new List<int> { 3, 4 }, values);
+		}
+
+		[Fact]
+		public void Take_試す() {
+			// Arrange
+			var values = new List<int>();
+
+			// Act
+			Observable.Range(0, 5)
+				.Take(3)
+				.Subscribe(value => values.Add(value));
+
+			// Assert
+			Assert.Equal(new List<int> { 0, 1, 2 }, values);
+		}
 	}
 }
