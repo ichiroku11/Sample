da31ebe3347d4895471027f82d283505922ad6b0
ichiroku11 <toolbox0x10@gmail.com>
2020-08-22 22:41:13 +0900

    SampleTest: Test Observable.Zip

--- src/DotNetCore3x/SampleTest/Reactive/ObservableCombiningTest.cs
+++ src/DotNetCore3x/SampleTest/Reactive/ObservableCombiningTest.cs
@@ -19,5 +19,39 @@ namespace SampleTest.Reactive {
 			// Assert
 			Assert.Equal(new List<int> { 1, 2, 3 }, values);
 		}
+
+		[Fact]
+		public void Zip_シーケンスをマージする() {
+			// Arrange
+			var values = new List<int>();
+
+			// Act
+			Observable.Range(1, 3)
+				.Zip(
+					Observable.Repeat(2, 3),
+					(first, second) => first + second)
+				.Subscribe(value => values.Add(value));
+
+			// Assert
+			Assert.Equal(new List<int> { 3, 4, 5 }, values);
+		}
+
+		[Fact]
+		public void Zip_どちらかのシーケンスが完了した段階で完了する() {
+			// Arrange
+			var values = new List<int>();
+
+			// Act
+			// 1つ目はシーケンスは3つ発行して完了
+			// 2つ目のシーケンスは2つ発行して完了
+			Observable.Range(1, 3)
+				.Zip(
+					Observable.Repeat(2, 2),
+					(first, second) => first + second)
+				.Subscribe(value => values.Add(value));
+
+			// Assert
+			Assert.Equal(new List<int> { 3, 4 }, values);
+		}
 	}
 }
