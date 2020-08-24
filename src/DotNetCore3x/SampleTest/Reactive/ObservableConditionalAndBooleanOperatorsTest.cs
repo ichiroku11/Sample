ce36e288acad05e36f61211a14c1974bfb24c6a1
ichiroku11 <toolbox0x10@gmail.com>
2020-08-24 13:22:29 +0900

    SampleTest: Test Observable.Any

--- src/DotNetCore3x/SampleTest/Reactive/ObservableConditionalAndBooleanOperatorsTest.cs
+++ src/DotNetCore3x/SampleTest/Reactive/ObservableConditionalAndBooleanOperatorsTest.cs
@@ -21,5 +21,25 @@ namespace SampleTest.Reactive {
 			// Assert
 			Assert.True(values.Single());
 		}
+
+		// 含んでいるかどうか
+		// http://reactivex.io/documentation/operators/contains.html
+		[Fact]
+		public void Any_発行された特定のアイテムが条件を満たすかどうかを判断する() {
+			// Arrange
+			var intercepts = new List<int>();
+			var values = new List<bool>();
+
+			// Act
+			Observable.Range(0, 5)
+				.Do(value => intercepts.Add(value))
+				.Any(value => value == 1)
+				.Subscribe(value => values.Add(value));
+
+			// Assert
+			// 条件を満たした段階で発行が終了するっぽい
+			Assert.Equal(new List<int> { 0, 1 }, intercepts);
+			Assert.True(values.Single());
+		}
 	}
 }
