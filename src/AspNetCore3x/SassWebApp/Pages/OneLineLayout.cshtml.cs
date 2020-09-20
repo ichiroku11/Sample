1c0facea610852542728de95c7dd7d35b0e2addc
ichiroku11 <toolbox0x10@gmail.com>
2020-09-20 10:17:15 +0900

    SassWebApp: Update one line layout

--- src/AspNetCore3x/SassWebApp/Pages/OneLineLayout.cshtml.cs
+++ src/AspNetCore3x/SassWebApp/Pages/OneLineLayout.cshtml.cs
@@ -9,6 +9,7 @@ namespace SassWebApp.Pages {
 	public enum OneLineLayout {
 		SuperCentered = 1,
 		DeconstructedPancake,
+		SidebarSays,
 	}
 
 	public static class OneLineLayoutExtensions {
@@ -17,6 +18,7 @@ namespace SassWebApp.Pages {
 			return layout switch {
 				OneLineLayout.SuperCentered => "01. Super Centered: place-items: center",
 				OneLineLayout.DeconstructedPancake => "02. The Deconstructed Pancake: flex: <grow> <shrink> <baseWidth>",
+				OneLineLayout.SidebarSays => "03. Sidebar Says: grid-template-columns: minmax(<min>, <max>) ...)",
 				_ => throw new InvalidOperationException(),
 			};
 		}
