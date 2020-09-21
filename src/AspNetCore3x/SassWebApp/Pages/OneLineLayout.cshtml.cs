ff370b241b996f00c2d05ed294b335e63d81ce29
ichiroku11 <toolbox0x10@gmail.com>
2020-09-21 09:54:43 +0900

    SassWebApp: Update one line layout

--- src/AspNetCore3x/SassWebApp/Pages/OneLineLayout.cshtml.cs
+++ src/AspNetCore3x/SassWebApp/Pages/OneLineLayout.cshtml.cs
@@ -23,16 +23,16 @@ namespace SassWebApp.Pages {
 		public static string GetDisplayName(this OneLineLayout layout) {
 			// 雑・・・
 			return layout switch {
-				OneLineLayout.SuperCentered => "01. Super Centered: place-items: center",
-				OneLineLayout.DeconstructedPancake => "02. The Deconstructed Pancake: flex: <grow> <shrink> <baseWidth>",
-				OneLineLayout.SidebarSays => "03. Sidebar Says: grid-template-columns: minmax(<min>, <max>) ...)",
-				OneLineLayout.PancakeStack => "04. Pancake Stack: grid-template-rows: auto 1fr auto",
-				OneLineLayout.ClassicHolyGrailLayout => "05. Classic Holy Grail Layout: grid-template: auto 1fr auto / auto 1fr auto",
-				OneLineLayout.TwelveSpanGrid => "06. 12-Span Grid: grid-template-columns: repeat(12, 1fr)",
-				OneLineLayout.RepeatAutoMinMax => "07. RAM (Repeat, Auto, MinMax): grid-template-columns(auto-fit, minmax(<base>, 1fr))",
-				OneLineLayout.LineUp => "08. Line Up: justify-content: space-between",
-				OneLineLayout.ClampingMyStyle => "09. Clamping My Style: clamp(<min>, <actual>, <max>)",
-				OneLineLayout.RespectForAspect => "10. Respect for Aspect: aspect-ratio: <width> / <height>",
+				OneLineLayout.SuperCentered => "01. 上下中央揃え: place-items: center",
+				OneLineLayout.DeconstructedPancake => "02. パンケーキ解体レイアウト: flex: grow | shrink | baseWidth",
+				OneLineLayout.SidebarSays => "03. サイドバー付きレイアウト: grid-template-columns: minmax(, ) …)",
+				OneLineLayout.PancakeStack => "04. パンケーキ重ねレイアウト: grid-template-rows: auto 1fr auto",
+				OneLineLayout.ClassicHolyGrailLayout => "05. 聖杯レイアウト: grid-template: auto 1 fr auto / auto 1fr auto",
+				OneLineLayout.TwelveSpanGrid => "06. 12カラムグリッド: grid-template-columns: repeat(12, 1fr)",
+				OneLineLayout.RepeatAutoMinMax => "07. RAM(Repeat, Auto, MinMax): grid-template-columns(auto-fit, minmax(, 1fr))",
+				OneLineLayout.LineUp => "08. 要素の整列: justify-content: space-between",
+				OneLineLayout.ClampingMyStyle => "09. clamp(最小, 実サイズ, 最大)",
+				OneLineLayout.RespectForAspect => "10. 画像などのアスペクト比を維持: aspect-ratio: 幅 / 高さ",
 				_ => throw new InvalidOperationException(),
 			};
 		}
