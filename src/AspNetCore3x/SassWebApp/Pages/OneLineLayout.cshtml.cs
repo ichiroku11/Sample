39fdc60c7e9c4ec7b7a024ed1a101384ccc74779
ichiroku11 <toolbox0x10@gmail.com>
2020-09-21 09:50:58 +0900

    SassWebApp: Update one line layout

--- src/AspNetCore3x/SassWebApp/Pages/OneLineLayout.cshtml.cs
+++ src/AspNetCore3x/SassWebApp/Pages/OneLineLayout.cshtml.cs
@@ -11,6 +11,12 @@ namespace SassWebApp.Pages {
 		DeconstructedPancake,
 		SidebarSays,
 		PancakeStack,
+		ClassicHolyGrailLayout,
+		TwelveSpanGrid,
+		RepeatAutoMinMax,
+		LineUp,
+		ClampingMyStyle,
+		RespectForAspect,
 	}
 
 	public static class OneLineLayoutExtensions {
@@ -21,6 +27,12 @@ namespace SassWebApp.Pages {
 				OneLineLayout.DeconstructedPancake => "02. The Deconstructed Pancake: flex: <grow> <shrink> <baseWidth>",
 				OneLineLayout.SidebarSays => "03. Sidebar Says: grid-template-columns: minmax(<min>, <max>) ...)",
 				OneLineLayout.PancakeStack => "04. Pancake Stack: grid-template-rows: auto 1fr auto",
+				OneLineLayout.ClassicHolyGrailLayout => "05. Classic Holy Grail Layout: grid-template: auto 1fr auto / auto 1fr auto",
+				OneLineLayout.TwelveSpanGrid => "06. 12-Span Grid: grid-template-columns: repeat(12, 1fr)",
+				OneLineLayout.RepeatAutoMinMax => "07. RAM (Repeat, Auto, MinMax): grid-template-columns(auto-fit, minmax(<base>, 1fr))",
+				OneLineLayout.LineUp => "08. Line Up: justify-content: space-between",
+				OneLineLayout.ClampingMyStyle => "09. Clamping My Style: clamp(<min>, <actual>, <max>)",
+				OneLineLayout.RespectForAspect => "10. Respect for Aspect: aspect-ratio: <width> / <height>",
 				_ => throw new InvalidOperationException(),
 			};
 		}
