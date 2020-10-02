a380f994cc1e4c0b24f11d57ab9fcc5cdc04c124
ichiroku11 <toolbox0x10@gmail.com>
2020-10-02 23:55:34 +0900

    ModelBindingWebApp: Add polymorphic model binding sample

---
+++ src/AspNetCore3x/ModelBindingWebApp/Controllers/GeometryController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelBindingWebApp.Models;
+
+namespace ModelBindingWebApp.Controllers {
+	// ポリモーフィズムを使ったモデルバインディングのサンプル
+	// https://docs.microsoft.com/ja-jp/aspnet/core/mvc/advanced/custom-model-binding?view=aspnetcore-3.1#polymorphic-model-binding
+	public class GeometryController : Controller {
+		public IActionResult Index() {
+			return View();
+		}
+
+		public IActionResult Save(GeometryModel model) {
+			return new EmptyResult();
+		}
+	}
+}
