db71c0d14662c518de43eef23e1b179eae46ef02
ichiroku11 <toolbox0x10@gmail.com>
2020-10-07 16:52:00 +0900

    SampleTest: Update EnumerableSelectManyTest

--- src/DotNetCore3x/SampleTest/Linq/EnumerableSelectManyTest.cs
+++ src/DotNetCore3x/SampleTest/Linq/EnumerableSelectManyTest.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace SampleTest.Linq {
 	public class EnumerableSelectManyTest {
+		private readonly ITestOutputHelper _output;
+
+		public EnumerableSelectManyTest(ITestOutputHelper output) {
+			_output = output;
+		}
+
 		[Fact]
 		public void SelectMany_配列の配列を平坦化するサンプル() {
 			// Arrange
@@ -20,6 +27,35 @@ namespace SampleTest.Linq {
 			// Assert
 			var expected = new[] {
 				1, 2, 3, 4, 5, 6
+			};
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void SelectMany_2重ループを平坦化で表現する() {
+			// Arrange
+			var source1 = Enumerable.Range(0, 3);
+			var source2 = Enumerable.Range(1, 2);
+
+			// Act
+			var actual = source1.SelectMany(x => source2, (x, y) => (x, y));
+
+			// こう書きたい
+			foreach (var (x, y) in actual) {
+				_output.WriteLine($"({x}, {y})");
+			}
+			// 以下と同じイメージ
+			foreach (var x in source1) {
+				foreach (var y in source2) {
+					_output.WriteLine($"({x}, {y})");
+				}
+			}
+
+			// Assert
+			var expected = new[] {
+				(0, 1), (0, 2),
+				(1, 1), (1, 2),
+				(2, 1), (2, 2),
 			};
 			Assert.Equal(expected, actual);
 		}
