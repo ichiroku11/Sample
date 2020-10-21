81d213d727797f3aae0a5c91c3ed2219f7f6f5e9
ichiroku11 <toolbox0x10@gmail.com>
2020-10-21 23:14:39 +0900

    TypeScriptWebApp: Update data url sample

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/misc/imagedataurl.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/misc/imagedataurl.ts
@@ -5,19 +5,48 @@ document.addEventListener("DOMContentLoaded", _ => {
 		return;
 	}
 
-	droparea.addEventListener("dragenter", _ => {
-		console.log("dragenter");
+	// https://developer.mozilla.org/ja/docs/DragDrop/Drag_Operations
+	// dragenter および dragover イベントのどちらにおいても、
+	// preventDefault() メソッドを呼び出すと、
+	// その場所がドロップ可能な場所であるということを示します
+
+	droparea.addEventListener("dragenter", event => {
+		droparea.classList.add("dragging");
+
+		// dragenterイベントとdragoverイベントでpreventDefault()メソッドを呼び出すとdropイベントが発生する
+		event.preventDefault();
 	});
 
-	droparea.addEventListener("dragover", _ => {
-		console.log("dragover");
+	droparea.addEventListener("dragover", event => {
+		event.preventDefault();
 	});
 
 	droparea.addEventListener("dragleave", _ => {
-		console.log("dragleave");
+		droparea.classList.remove("dragging");
 	});
 
-	droparea.addEventListener("drop", _ => {
-		console.log("drop");
+	droparea.addEventListener("drop", event => {
+		droparea.classList.remove("dragging");
+
+		event.preventDefault();
+
+		if (!(event instanceof DragEvent)) {
+			return;
+		}
+
+		var files = event.dataTransfer?.files;
+		if (!files) {
+			return;
+		}
+		if (files.length < 1) {
+			return;
+		}
+
+		var file = files[0];
+
+		// todo:
+		console.log(file.name);
+		console.log(file.type);
+		console.log(file.size);
 	});
 });
