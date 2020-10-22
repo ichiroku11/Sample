1422715be6c513bbc70b0353bf80f867752e3bf2
ichiroku11 <toolbox0x10@gmail.com>
2020-10-22 21:44:41 +0900

    TypeScriptWebApp: Update data url sample

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/misc/imagedataurl.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/misc/imagedataurl.ts
@@ -1,7 +1,21 @@
 
+function readAsDataUrl(blob: Blob): Promise<string> {
+	return new Promise<string>((resolve, reject) => {
+		const reader = new FileReader();
+		reader.addEventListener("load", _ => resolve(reader.result as string));
+		reader.addEventListener("error", _ => reject(reader.error));
+		reader.readAsDataURL(blob);
+	});
+}
+
 document.addEventListener("DOMContentLoaded", _ => {
 	const droparea = document.querySelector("#droparea");
 	if (!droparea) {
+		return;
+	}
+
+	const result = document.querySelector("#result");
+	if (!result) {
 		return;
 	}
 
@@ -25,7 +39,7 @@ document.addEventListener("DOMContentLoaded", _ => {
 		droparea.classList.remove("dragging");
 	});
 
-	droparea.addEventListener("drop", event => {
+	droparea.addEventListener("drop", async event => {
 		droparea.classList.remove("dragging");
 
 		event.preventDefault();
@@ -43,10 +57,13 @@ document.addEventListener("DOMContentLoaded", _ => {
 		}
 
 		var file = files[0];
+		const dataUrl = await readAsDataUrl(file);
 
-		// todo:
-		console.log(file.name);
-		console.log(file.type);
-		console.log(file.size);
+		while (result.firstChild) {
+			result.removeChild(result.firstChild);
+		}
+		const image = document.createElement("img");
+		image.src = dataUrl;
+		result.append(image);
 	});
 });
