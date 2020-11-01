b37ade6f8d9e78e4013016b8e3424aa69e21d78a
ichiroku11 <toolbox0x10@gmail.com>
2020-11-01 15:16:43 +0900

    TypeScriptWebApp: Update set test

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/test/set.test.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/test/set.test.ts
@@ -2,6 +2,14 @@ import { Assert, Test } from "../unittestlib";
 
 
 export const setTest = new Test("SetTest")
+	.fact("constructor_引数に配列を渡せる", () => {
+		// Arrange
+		// Act
+		const actual = new Set([1, 2]);
+
+		// Assert
+		Assert.equal([1, 2], Array.from(actual));
+	})
 	.fact("add_重複した要素は追加されないし追加しても例外にならない", () => {
 		// Arrange
 		const actual = new Set<number>();
@@ -13,4 +21,4 @@ export const setTest = new Test("SetTest")
 
 		// Assert
 		Assert.equal([1], Array.from(actual));
-	})
+	});
