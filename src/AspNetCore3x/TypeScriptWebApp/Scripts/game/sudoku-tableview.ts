d8556b9e019d2bb0ed794637401ed8cbc8776369
ichiroku11 <toolbox0x10@gmail.com>
2020-11-06 07:56:03 +0900

    TypeScriptWebApp: Update sudoku sample

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/game/sudoku-tableview.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/game/sudoku-tableview.ts
@@ -1,5 +1,14 @@
 import { SudokuComponent, SudokuDefault, SudokuUndefinedOrDigit } from "./sudoku-helper";
 
+type Range = (start: number, count: number) => number[];
+
+/**
+ * 指定した範囲内の整数の配列を生成する
+ * @param start 最初の値
+ * @param count 生成する整数の数
+ */
+const range: Range = (start, count) => Array.from({ length: count }, (_, index) => start + index);
+
 /*
  * 数独table要素ビュー
  */
@@ -7,13 +16,25 @@ export class SudokuTableView {
 	private readonly _table: HTMLTableElement;
 
 	public constructor(selector: string) {
-		const table = document.querySelector<HTMLTableElement>(`${selector} table`);
-		if (!table) {
-			// todo: 例外よいか？
-			throw new Error();
-		}
+		this._table = this.create();
 
-		this._table = table;
+		document.querySelector(selector)?.appendChild(this._table);
+	}
+
+	private create(): HTMLTableElement {
+		const table = document.createElement("table");
+		const tbody = document.createElement("tbody");
+		for (const _ of range(0, 9)) {
+			const tr = document.createElement("tr");
+			for (const _ of range(0, 9)) {
+				const td = document.createElement("td");
+				tr.appendChild(td);
+			}
+			tbody.appendChild(tr);
+		}
+		table.appendChild(tbody);
+
+		return table;
 	}
 
 	public update(x: SudokuComponent, y: SudokuComponent, value: SudokuUndefinedOrDigit, fixed = false) {
