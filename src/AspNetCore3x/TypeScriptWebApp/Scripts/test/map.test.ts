0b3cab0a2a8d30677e5ab49befe4e4403509f3a6
ichiroku11 <toolbox0x10@gmail.com>
2020-11-11 08:01:35 +0900

    TypeScriptWebApp: Test Map object

--- src/AspNetCore3x/TypeScriptWebApp/Scripts/test/map.test.ts
+++ src/AspNetCore3x/TypeScriptWebApp/Scripts/test/map.test.ts
@@ -4,8 +4,13 @@ import { Assert, Test } from "../unittestlib";
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Map
 
 export const mapTest = new Test("MapTest")
-	.fact("", () => {
+	.fact("constructor_引数に[key, value]の配列を渡せる", () => {
 		// Arrange
 		// Act
+		const map = new Map([[1, "x"], [2, "y"]]);
+
 		// Assert
+		Assert.equal(2, map.size);
+		Assert.equal("x", map.get(1));
+		Assert.equal("y", map.get(2));
 	});
